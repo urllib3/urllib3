fccbeb6cc0b104425fa0b7b541cfe5879a8afe4f
Andrey Petrov <andrey.petrov@shazow.net>
2010-07-13 15:37:11 -0700

    Adding metadata to module.

--- urllib3/__init__.py
+++ urllib3/__init__.py
@@ -1,3 +1,11 @@
+"""
+urllib3 - Thread-safe connection pooling and re-using.
+"""
+
+__author__ = "Andrey Petrov (andrey.petrov@shazow.net)"
+__license__ = "MIT"
+__version__ = "$Rev$"
+
 from connectionpool import HTTPConnectionPool, HTTPSConnectionPool, get_host, connection_from_url
 from filepost import encode_multipart_formdata
 
