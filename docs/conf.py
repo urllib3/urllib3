c5ee732d687aad5760429bcb7e8fd49103998d22
Andrey Petrov <andrey.petrov@shazow.net>
2012-01-07 15:02:41 -0800

    2011 -> 2012

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'urllib3'
-copyright = u'2011, Andrey Petrov'
+copyright = u'2012, Andrey Petrov'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
