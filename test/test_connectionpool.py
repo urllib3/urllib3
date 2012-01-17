e0ee3a26bf2528063eeb0695579e265cc66570d7
Brandon Rhodes <brandon@rhodesmill.org>
2012-01-17 16:56:18 -0500

    Added a test (that succeeds!) of whether connection pools survive dead sockets.

--- test/test_connectionpool.py
+++ test/test_connectionpool.py
@@ -1,4 +1,7 @@
 import unittest
+import socket
+from threading import Event, Thread
+from Queue import Queue
 
 from urllib3.connectionpool import (
     connection_from_url,
@@ -92,7 +95,65 @@ class TestConnectionPool(unittest.TestCase):
 
         self.assertEqual(pool.num_connections, 1)
 
+    def test_recovery_when_server_closes_connection(self):
+        # Does the pool work seamlessly if an open connection in the
+        # connection pool gets hung up on by the server, then reaches
+        # the front of the queue again?
 
+        def server(listener):
+            for i in 0, 1:
+                sock = listener.accept()[0]
+                read_request(sock)
+                body = 'Response %d' % i
+                sock.send('HTTP/1.1 200 OK\r\n'
+                          'Content-Type: text/plain\r\n'
+                          'Content-Length: %d\r\n'
+                          '\r\n'
+                          '%s' % (len(body), body))
+                sock.close()  # simulate a server timing out, closing socket
+                done_closing.set()  # let the test know it can proceed
+
+        done_closing = Event()
+        address = start_server(server)
+        pool = HTTPConnectionPool(*address)
+
+        response = pool.request('GET', '/', retries=0)
+        self.assertEqual(response.status, 200)
+        self.assertEqual(response.data, 'Response 0')
+
+        done_closing.wait()  # wait until the socket in our pool gets closed
+
+        response = pool.request('GET', '/', retries=0)
+        self.assertEqual(response.status, 200)
+        self.assertEqual(response.data, 'Response 1')
+
+# Prepare to serve HTTP on localhost.
+
+def read_request(sock):
+    """Read `sock` until a double CR-LF, and return the data received."""
+    s = ''
+    while not s.endswith('\r\n\r\n'):
+        s += sock.recv(65536)
+    return s
+
+def start_server(server_function):
+    """Create a listening server socket and publish its port on a queue."""
+
+    def server_thread():
+        sock = socket.socket()
+        sock.bind(('127.0.0.1', 0))
+        address = sock.getsockname()
+        sock.listen(1)  # Once listen() returns, the server socket is ready
+        q.put(address)     # ... so we can safely tell the client to use it now
+        server_function(sock)
+
+    q = Queue()
+    t = Thread(target=server_thread)
+    t.daemon = True
+    t.start()
+    return q.get()  # Wait until the server has started up, then return.
+
+# "python -m" command line support:
 
 if __name__ == '__main__':
     unittest.main()
