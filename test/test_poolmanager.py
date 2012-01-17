6cd6854b4cf6bb09ad7ab84d115f5c942cd25503
Brandon Rhodes <brandon@rhodesmill.org>
2012-01-17 17:40:17 -0500

    Added a failing test case for github issue #28.

--- test/test_poolmanager.py
+++ test/test_poolmanager.py
@@ -1,8 +1,10 @@
 import unittest
 
-from urllib3.poolmanager import PoolManager
+from urllib3.poolmanager import (PoolManager, pool_classes_by_scheme,
+                                 port_by_scheme)
 from urllib3 import connection_from_url
 
+from test_connectionpool import read_request, start_server
 
 class TestPoolManager(unittest.TestCase):
     def test_same_url(self):
@@ -41,6 +43,31 @@ class TestPoolManager(unittest.TestCase):
             connections.add(conn)
 
         self.assertEqual(len(connections), 5)
+
+    def test_request_survives_missing_port_number(self):
+        # Can a URL that lacks an explicit port like ':80' succeed, or
+        # will all such URLs fail with an error?
+
+        def server(listener):
+            sock = listener.accept()[0]
+            read_request(sock)
+            sock.send('HTTP/1.1 200 OK\r\n'
+                      'Content-Type: text/plain\r\n'
+                      'Content-Length: 8\r\n'
+                      '\r\n'
+                      'Inspire.')
+            sock.close()
+
+        # We pretend, for a moment, that HTTP lives on the port at which
+        # our test server happens to be listening.
+
+        p = PoolManager()
+        host, port = start_server(server)
+        port_by_scheme['http'] = port
+        try:
+            p.request('GET', 'http://%s/' % host, retries=0)
+        finally:
+            port_by_scheme['http'] = 80
 
 
 if __name__ == '__main__':
