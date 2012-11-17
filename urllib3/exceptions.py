71316c51d989998e01e94102f98298cc1fc62105
Russell Ballestrini <fox@blanka.foxhop.net>
2012-11-17 11:24:42 -0500

    Patch for issue #116, Setting timeout on celery worker causes TypeError when attempting to throw TimeoutError.

--- urllib3/exceptions.py
+++ urllib3/exceptions.py
@@ -14,7 +14,7 @@ class HTTPError(Exception):
 
 class PoolError(HTTPError):
     "Base exception for errors caused within a pool."
-    def __init__(self, pool, message):
+    def __init__(self, pool, message=""):
         self.pool = pool
         HTTPError.__init__(self, "%s: %s" % (pool, message))
 
