823f8d57807b67783e89cb5cc79573dc2b82397e
Sune Kirkeby <mig@ibofobi.dk>
2013-03-20 20:11:46 +0100

    Add inject_into_urllib3 code snippet.

--- urllib3/contrib/pyopenssl.py
+++ urllib3/contrib/pyopenssl.py
@@ -6,9 +6,18 @@ This needs the following packages installed:
 * ndg-httpsclient (tested with 0.3.2)
 * pyasn1 (tested with 0.1.6)
 
-To activate it call :func:`urllib3.contrib.pyopenssl.inject_into_urllib3`.
+To activate it call :func:`~urllib3.contrib.pyopenssl.inject_into_urllib3`.
 This can be done in a ``sitecustomize`` module, or at any other time before
-your application begins using ``urllib3``.
+your application begins using ``urllib3``, like this::
+
+    try:
+        import urllib3.contrib.pyopenssl
+        urllib3.contrib.pyopenssl.inject_into_urllib3()
+    except ImportError:
+        pass
+
+Now you can use :mod:`urllib3` as you normally would, and it will support SNI
+when the required modules are installed.
 '''
 
 from ndg.httpsclient.ssl_peer_verification import (ServerSSLCertVerification,
