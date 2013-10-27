09d03c9a75ba81d4469920b2d91df6d3ab4d7499
Cory Benfield <lukasaoz@gmail.com>
2013-10-27 07:33:46 +0000

    Stop applying special headers to proxies.

--- test/test_proxymanager.py
+++ test/test_proxymanager.py
@@ -4,34 +4,6 @@ from urllib3.poolmanager import ProxyManager
 
 
 class TestProxyManager(unittest.TestCase):
-    def test_proxy_headers(self):
-        p = ProxyManager('http://something:1234')
-        url = 'http://pypi.python.org/test'
-
-        # Verify default headers
-        default_headers = {'Accept': '*/*',
-                           'Host': 'pypi.python.org'}
-        headers = p._set_proxy_headers(url)
-
-        self.assertEqual(headers, default_headers)
-
-        # Verify default headers don't overwrite provided headers
-        provided_headers = {'Accept': 'application/json',
-                            'custom': 'header',
-                            'Host': 'test.python.org'}
-        headers = p._set_proxy_headers(url, provided_headers)
-
-        self.assertEqual(headers, provided_headers)
-
-        # Verify proxy with nonstandard port
-        provided_headers = {'Accept': 'application/json'}
-        expected_headers = provided_headers.copy()
-        expected_headers.update({'Host': 'pypi.python.org:8080'})
-        url_with_port = 'http://pypi.python.org:8080/test'
-        headers = p._set_proxy_headers(url_with_port, provided_headers)
-
-        self.assertEqual(headers, expected_headers)
-
     def test_default_port(self):
         p = ProxyManager('http://something')
         self.assertEqual(p.proxy.port, 80)
