38e5bec11f807e2dfeb150797a471a2199e29c49
Toshio Kuratomi <toshio@fedoraproject.org>
2013-10-28 18:17:45 -0700

    Make a note of ssl_match_hostname being under the PSF license

--- urllib3/packages/ssl_match_hostname/__init__.py
+++ urllib3/packages/ssl_match_hostname/__init__.py
@@ -1,5 +1,8 @@
 """The match_hostname() function from Python 3.3.3, essential when using SSL."""
 
+# Note: This file is under the PSF license as the code comes from the python
+# stdlib.   http://docs.python.org/3/license.html
+
 import re
 
 __version__ = '3.4.0.2'
