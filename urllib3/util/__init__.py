f8976bdd8c7dbf08768f7a5efd16d0a9afec0d2e
Andrey Petrov <andrey.petrov@shazow.net>
2014-03-15 17:20:35 -0700

    util.conn -> util.connection and import cleanup

--- urllib3/util/__init__.py
+++ urllib3/util/__init__.py
@@ -4,30 +4,21 @@
 # This module is part of urllib3 and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
 
-# For backwards compatibility, allow you to access resources that used to exist
-# here.
-from .conn import is_connection_dropped
-
+from .connection import is_connection_dropped
 from .request import make_headers
-
 from .response import is_fp_closed
-
 from .ssl_ import (
+    SSLContext,
+    HAS_SNI,
     assert_fingerprint,
     resolve_cert_reqs,
     resolve_ssl_version,
     ssl_wrap_socket,
 )
-try:
-    from .ssl_ import SSLContext, HAS_SNI
-except ImportError:
-    pass
-
 from .timeout import (
     current_time,
     Timeout,
 )
-
 from .url import (
     get_host,
     parse_url,
