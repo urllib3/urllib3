debf960b8ac21866f2c390339372eea5c6db7dcf
Andrey Petrov <andrey.petrov@shazow.net>
2014-06-23 16:39:58 -0700

    release.sh

--- release.sh
+++ release.sh
@@ -12,7 +12,24 @@ fi
 
 git merge master --no-commit
 git checkout master -- CHANGES.rst
-sed -i '' '4,9d' CHANGES.rst
 
-version="$(grep -m1 -B1 '+++++' "$CHANGES_FILE" | head -n1 | cut -d' ' -f1)"
-perl -p -i -e "s/__version__.*/__version__ = '$(version)'/" "$(VERSION_FILE)"
+# Remove dev section and update version.
+sed -i '' '4,9d' CHANGES.rst
+version="$(grep -m1 -B1 '+++++' "${CHANGES_FILE}" | head -n1 | cut -d' ' -f1)"
+perl -p -i -e "s/__version__.*/__version__ = '${version}'/" "${VERSION_FILE}"
+git diff
+
+read -n1 -p "Good? [Y/n] " r
+if ! [[ $r =~ ^([yY])$ ]]; then
+    echo "Stopped."
+    exit 2
+fi
+
+git commit -a -m 'Merging new release version: ${version}'
+git tag ${version}
+python setup.py sdist
+
+echo "Release is ready. Publish it when ready:"
+echo "git push origin"
+echo "python setup.py sdist upload"
+
