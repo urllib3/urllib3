b88cc6d310ba0c07f452caec7c926d149539220d
James Salter <jsalter@iress.com.au>
2014-07-30 17:42:29 +1000

    is_fp_closed checks .closed first

--- urllib3/util/response.py
+++ urllib3/util/response.py
@@ -5,9 +5,16 @@ def is_fp_closed(obj):
     :param obj:
         The file-like object to check.
     """
+
+    try:
+        # check via the official file-like-object way
+        return obj.closed
+    except AttributeError:
+        pass
+
     if hasattr(obj, 'fp'):
         # Object is a container for another file-like object that gets released
         # on exhaustion (e.g. HTTPResponse)
         return obj.fp is None
 
-    return obj.closed
+    raise ValueError("unable to determine whether fp is closed")
