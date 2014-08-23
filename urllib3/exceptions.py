1bc3c35d5942b184196f4370010130a50166af62
Kevin Burke <kev@inburke.com>
2014-08-23 13:59:13 -0700

    Document MaxRetryError parameters.

--- urllib3/exceptions.py
+++ urllib3/exceptions.py
@@ -60,7 +60,14 @@ ConnectionError = ProtocolError
 ## Leaf Exceptions
 
 class MaxRetryError(RequestError):
-    "Raised when the maximum number of retries is exceeded."
+    """Raised when the maximum number of retries is exceeded.
+
+    :param pool: The connection pool
+    :type pool: :class:`~urllib3.connectionpool.HTTPConnectionPool`
+    :param string url: The requested Url
+    :param exceptions.Exception reason: The underlying error
+
+    """
 
     def __init__(self, pool, url, reason=None):
         self.reason = reason
