707e98022b029dd5af4ca4f3493182fafe7d789b
Thomas Weißschuh <thomas@t-8ch.de>
2014-08-24 11:57:42 +0000

    test everything on pyopenssl

--- test/contrib/test_pyopenssl.py
+++ test/contrib/test_pyopenssl.py
@@ -1,9 +1,5 @@
-try:
-    from urllib3.contrib.pyopenssl import (inject_into_urllib3,
-                                           extract_from_urllib3)
-except ImportError as e:
-    from nose.plugins.skip import SkipTest
-    raise SkipTest('Could not import pyopenssl: %r' % e)
+from urllib3.contrib.pyopenssl import (inject_into_urllib3,
+                                       extract_from_urllib3)
 
 from ..with_dummyserver.test_https import TestHTTPS, TestHTTPS_TLSv1
 from ..with_dummyserver.test_socketlevel import TestSNI, TestSocketClosing
