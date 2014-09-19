af07edc34eb1161fd98172880f2fc6ff5b35b8a5
Ian Cordasco <ian.cordasco@rackspace.com>
2014-09-19 11:22:08 -0500

    Fix usage of CERT_NONE with an SSLContext
    Don't update the SSLContext.protocol version since the value is only
    important in SSLContext.__new__ and it will not have the desired effect

--- urllib3/util/ssl_.py
+++ urllib3/util/ssl_.py
@@ -111,9 +111,11 @@ if SSLContext is not None:  # Python 3.2+
             OP_NO_COMPRESSION = 0x20000
             context.options |= OP_NO_COMPRESSION
         else:  # Python 3.4+
-            context = create_default_context()
-            # This is a really bad idea
-            context.protocol = ssl_version
+            if cert_reqs == CERT_NONE:
+                # If we don't pass None, we can't set verify_mode to CERT_NONE
+                context = create_default_context(None)
+            else:
+                context = create_default_context()
             # Python 3.4 automatically turns off compression so we don't need to.
 
         context.verify_mode = cert_reqs
