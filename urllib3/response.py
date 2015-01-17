b894fe16b474c976bd92a2dc1a77cd5b6dc7fb2e
Bryan Helmig <bryan@bryanhelmig.com>
2015-01-16 17:19:53 -0800

    when decompressing (gzip/deflate/etc.), ensure there is data before decompressing

--- urllib3/response.py
+++ urllib3/response.py
@@ -216,14 +216,14 @@ class HTTPResponse(io.IOBase):
             self._fp_bytes_read += len(data)
 
             try:
-                if decode_content and self._decoder:
+                if data and decode_content and self._decoder:
                     data = self._decoder.decompress(data)
             except (IOError, zlib.error) as e:
                 raise DecodeError(
                     "Received response with content-encoding: %s, but "
                     "failed to decode it." % content_encoding, e)
 
-            if flush_decoder and decode_content and self._decoder:
+            if data and flush_decoder and decode_content and self._decoder:
                 buf = self._decoder.decompress(binary_type())
                 data += buf + self._decoder.flush()
 
