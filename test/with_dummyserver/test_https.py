af13eadabbd821c9672b2dd65030b1e043a6a8d2
Kevin Burke <kev@inburke.com>
2015-04-06 10:30:52 -0700

    fix tests

--- test/with_dummyserver/test_https.py
+++ test/with_dummyserver/test_https.py
@@ -336,7 +336,7 @@ class TestHTTPS(HTTPSDummyServerTestCase):
         https_pool = new_pool(Timeout(connect=0.001))
         conn = https_pool._new_conn()
         self.assertRaises(ConnectTimeoutError, https_pool.request, 'GET', '/')
-        self.assertRaises(ConnectTimeoutError, https_pool._make_request, conn,
+        self.assertRaises(ConnectTimeoutError, https_pool._make_connect, conn,
                           'GET', '/')
 
         https_pool = new_pool(Timeout(connect=5))
@@ -357,6 +357,7 @@ class TestHTTPS(HTTPSDummyServerTestCase):
                 cert_reqs='CERT_REQUIRED', ca_certs=DEFAULT_CA,
                 assert_fingerprint=fingerprint)
 
+        conn = https_pool._make_connect(conn, 'GET', '/')
         https_pool._make_request(conn, 'GET', '/')
 
     def test_ssl_correct_system_time(self):
