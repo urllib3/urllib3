95b23902c4f239846f995012cccaace45330c28e
Cory Benfield <lukasaoz@gmail.com>
2015-08-31 08:29:34 +0100

    Test ca_cert_dir parameter.

--- test/test_util.py
+++ test/test_util.py
@@ -395,6 +395,14 @@ class TestUtil(unittest.TestCase):
         mock_context.load_verify_locations.assert_called_once_with(
             '/path/to/pem')
 
+    def test_ssl_wrap_socket_loads_certificate_directories(self):
+        socket = object()
+        mock_context = Mock()
+        ssl_wrap_socket(ssl_context=mock_context, ca_cert_dir='/path/to/pems',
+                        sock=socket)
+        mock_context.load_verify_locations.assert_called_once_with(
+            capath='/path/to/pems')
+
     def test_ssl_wrap_socket_with_no_sni(self):
         socket = object()
         mock_context = Mock()
