e93a40f999f80a65e7cdfb2c6c98dbbf3026425e
a-tal <github@talsma.ca>
2015-10-05 09:23:59 -0700

    allows user paths for ca_cert verify path

--- urllib3/connection.py
+++ urllib3/connection.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 import sys
 import socket
 from socket import error as SocketError, timeout as SocketTimeout
@@ -205,10 +206,12 @@ class VerifiedHTTPSConnection(HTTPSConnection):
         self.key_file = key_file
         self.cert_file = cert_file
         self.cert_reqs = cert_reqs
-        self.ca_certs = ca_certs
         self.ca_cert_dir = ca_cert_dir
         self.assert_hostname = assert_hostname
         self.assert_fingerprint = assert_fingerprint
+        if ca_certs:
+            ca_certs = os.path.expanduser(ca_certs)
+        self.ca_certs = ca_certs
 
     def connect(self):
         # Add certificate verification
