cc2d86fc13688e573721ee7a7571cb5315ef2017
Cory Benfield <lukasaoz@gmail.com>
2015-12-15 16:41:16 +0000

    Strip square brackets from hostnames before verifying

--- urllib3/connection.py
+++ urllib3/connection.py
@@ -265,7 +265,9 @@ class VerifiedHTTPSConnection(HTTPSConnection):
                     'for details.)'.format(hostname)),
                     SubjectAltNameWarning
                 )
-            match_hostname(cert, self.assert_hostname or hostname)
+            asserted_hostname = self.assert_hostname or hostname
+            asserted_hostname = asserted_hostname.strip('[]')
+            match_hostname(cert, asserted_hostname)
 
         self.is_verified = (resolved_cert_reqs == ssl.CERT_REQUIRED or
                             self.assert_fingerprint is not None)
