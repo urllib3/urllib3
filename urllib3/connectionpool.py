8e4c48328f502bc9cb302f5ada99ee434208dd8a
birm <birm@gatech.edu>
2016-06-20 17:38:55 -0400

    Add scheme to debug log

--- urllib3/connectionpool.py
+++ urllib3/connectionpool.py
@@ -397,8 +397,9 @@ class HTTPConnectionPool(ConnectionPool, RequestMethods):
 
         # AppEngine doesn't have a version attr.
         http_version = getattr(conn, '_http_vsn_str', 'HTTP/?')
-        log.debug("%s:%s \"%s %s %s\" %s %s", self.host, self.port, method, url, http_version,
-                  httplib_response.status, httplib_response.length)
+        log.debug("%s:%s \"%s %s %s\" %s %s using scheme %s ", self.host, self.port,
+                  method, url, http_version, httplib_response.status,
+                  httplib_response.length, self.scheme)
 
         try:
             assert_header_parsing(httplib_response.msg)
