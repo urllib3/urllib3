9f5454eac808a105307b2d363c99ce97e5109821
Cory Benfield <lukasaoz@gmail.com>
2016-07-11 09:48:10 +0100

    Have the 'secure' flag install ipaddress.
    This is actually strictly not needed: ipaddress is a transient
    dependency because cryptography requires it. However, it's best not to
    rely on transient dependencies for things *we* need!

--- setup.py
+++ setup.py
@@ -59,6 +59,9 @@ setup(name='urllib3',
               'pyasn1',
               'certifi',
           ],
+          'secure:python_version < "3.3"': [
+              "ipaddress",
+          ],
           'socks': [
               'PySocks>=1.5.6,<2.0',
           ]
