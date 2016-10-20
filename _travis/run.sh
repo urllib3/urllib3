d103b186270dc2ab32a47c8abd94abb861ac83e3
Cory Benfield <lukasaoz@gmail.com>
2016-10-20 12:41:50 +0100

    Keep using the good OpenSSL

--- _travis/run.sh
+++ _travis/run.sh
@@ -8,6 +8,10 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
     PYENV_ROOT="$HOME/.pyenv"
     PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
+
+    # Use the good OpenSSL
+    export LDFLAGS="-L$(brew --prefix openssl)/lib"
+    export CFLAGS="-I$(brew --prefix openssl)/include"
 fi
 
 tox
