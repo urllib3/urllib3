65c963232dd359e73b2311950c6835aed5eaf686
Tobias Megies <megies@geophysik.uni-muenchen.de>
2016-12-02 12:57:11 +0100

    refactoring of monkey patch resistance test case

--- test/test_queue_monkeypatch.py
+++ test/test_queue_monkeypatch.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import
 
+import mock
 import unittest
 
 import urllib3
 from urllib3.exceptions import EmptyPoolError
-import Queue
+from urllib3.packages.six.moves import queue
 
 class BadError(Exception):
     """
@@ -12,17 +13,17 @@ class BadError(Exception):
     """
     pass
 
-Queue.Empty = BadError
 
-
-class TestConnectionPool(unittest.TestCase):
+class TestMonkeypatchResistance(unittest.TestCase):
     """
+    Test that connection pool works even with a monkey patched Queue module,
+    see obspy/obspy#1599, kennethreitz/requests#3742, shazow/urllib3#1061.
     """
     def test_queue_monkeypatching(self):
-        http = urllib3.HTTPConnectionPool(host="localhost", block=True)
-        first_conn = http._get_conn(timeout=1)
-        with self.assertRaises(EmptyPoolError):
-            second_conn = http._get_conn(timeout=1)
+        with mock.patch.object(queue, 'Empty', BadError):
+            http = urllib3.HTTPConnectionPool(host="localhost", block=True)
+            first_conn = http._get_conn(timeout=1)
+            self.assertRaises(EmptyPoolError, http._get_conn, timeout=1)
 
 
 if __name__ == '__main__':
