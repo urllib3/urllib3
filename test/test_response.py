396b69e7a8b33c0689c3ad108913b8b347631e33
Cory Benfield <lukasaoz@gmail.com>
2017-01-19 09:21:44 +0000

    Buffering works now.

--- test/test_response.py
+++ test/test_response.py
@@ -295,8 +295,8 @@ class TestResponse(unittest.TestCase):
                          preload_content=False)
         stream = resp.stream(2)
 
-        self.assertEqual(next(stream), b'f')
-        self.assertEqual(next(stream), b'oo')
+        self.assertEqual(next(stream), b'fo')
+        self.assertEqual(next(stream), b'o')
         self.assertRaises(StopIteration, next, stream)
 
     def test_gzipped_streaming_tell(self):
