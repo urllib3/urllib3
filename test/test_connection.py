4e1554cc1697574b7797d39c0dbc1b6ed7364e72
Cory Benfield <lukasaoz@gmail.com>
2017-01-24 10:31:14 +0000

    Get test_connection working again.

--- test/test_connection.py
+++ test/test_connection.py
@@ -8,11 +8,10 @@ else:
 import mock
 
 from urllib3.connection import (
-    CertificateError,
     VerifiedHTTPSConnection,
-    _match_hostname,
     RECENT_DATE
 )
+from urllib3.util.ssl_ import CertificateError, match_hostname
 
 
 class TestConnection(unittest.TestCase):
@@ -22,24 +21,24 @@ class TestConnection(unittest.TestCase):
     def test_match_hostname_no_cert(self):
         cert = None
         asserted_hostname = 'foo'
-        self.assertRaises(ValueError, _match_hostname, cert, asserted_hostname)
+        self.assertRaises(ValueError, match_hostname, cert, asserted_hostname)
 
     def test_match_hostname_empty_cert(self):
         cert = {}
         asserted_hostname = 'foo'
-        self.assertRaises(ValueError, _match_hostname, cert, asserted_hostname)
+        self.assertRaises(ValueError, match_hostname, cert, asserted_hostname)
 
     def test_match_hostname_match(self):
         cert = {'subjectAltName': [('DNS', 'foo')]}
         asserted_hostname = 'foo'
-        _match_hostname(cert, asserted_hostname)
+        match_hostname(cert, asserted_hostname)
 
     def test_match_hostname_mismatch(self):
         cert = {'subjectAltName': [('DNS', 'foo')]}
         asserted_hostname = 'bar'
         try:
-            with mock.patch('urllib3.connection.log.error') as mock_log:
-                _match_hostname(cert, asserted_hostname)
+            with mock.patch('urllib3.util.ssl_.log.error') as mock_log:
+                match_hostname(cert, asserted_hostname)
         except CertificateError as e:
             self.assertEqual(str(e), "hostname 'bar' doesn't match 'foo'")
             mock_log.assert_called_once_with(
