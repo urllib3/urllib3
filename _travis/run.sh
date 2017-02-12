d32584fdffb82d18679bf1c7658f28ad500b99fa
Cory Benfield <lukasaoz@gmail.com>
2017-02-12 15:36:43 +0000

    Statically link OpenSSL and cryptography.
    This is necessary to avoid a mess due to distutils trying to replicate
    the compliation environment of the original Python. distutils loses
    track of what it's trying to do and so compiles using the header files
    from OpenSSL 1.1 but then links against OpenSSL 1.0, gets all in a
    tizzy, and throws its hands up in the air and quits in a huff.
    
    This avoids that by instead statically linking. It avoids getting the
    Python linker flags passed through and instead just finds the archive
    files directly. As an added benefit this is much more representative of
    how the cryptography wheels are distributed, which means that we can
    handily avoid testing in unrepresentative environments.
    
    Good work team!

--- _travis/run.sh
+++ _travis/run.sh
@@ -9,8 +9,10 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
     PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
 
-    # Use the good OpenSSL for PyOpenSSL.
-    export LDFLAGS="-L$(brew --prefix openssl@1.1)/lib"
+    # Use the good OpenSSL for PyOpenSSL. This also links it statically into
+    # cryptography, which more accurately reflects the way the wheel is used.
+    export CRYPTOGRAPHY_OSX_NO_LINK_FLAGS="1"
+    export LDFLAGS="$(brew --prefix openssl@1.1)/lib/libcrypto.a $(brew --prefix openssl@1.1)/lib/libssl.a"
     export CFLAGS="-I$(brew --prefix openssl@1.1)/include"
 fi
 
