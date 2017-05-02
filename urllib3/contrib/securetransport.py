c5a1a0553019521dba9fd7ebbed9e8ecbeb76615
Christian Heimes <christian@python.org>
2017-05-02 12:37:51 +0200

    Add TLS 1.3 cipher suites
    Signed-off-by: Christian Heimes <christian@python.org>

--- urllib3/contrib/securetransport.py
+++ urllib3/contrib/securetransport.py
@@ -91,6 +91,9 @@ SSL_WRITE_BLOCKSIZE = 16384
 # individual cipher suites. We need to do this becuase this is how
 # SecureTransport wants them.
 CIPHER_SUITES = [
+    SecurityConst.TLS_AES_256_GCM_SHA384,
+    SecurityConst.TLS_CHACHA20_POLY1305_SHA256,
+    SecurityConst.TLS_AES_128_GCM_SHA256,
     SecurityConst.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
     SecurityConst.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
     SecurityConst.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
