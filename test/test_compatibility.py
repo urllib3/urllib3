c7cbc4c16f9764ae148d079b51fc970dcdc028b5
Michael Löffler <michael@loeffler.io>
2017-07-17 14:07:59 +0200

    Add compatibility for py3 cookiejar

--- test/test_compatibility.py
+++ test/test_compatibility.py
@@ -3,6 +3,8 @@ import warnings
 import pytest
 
 from urllib3.connection import HTTPConnection
+from urllib3.response import HTTPResponse
+from urllib3.packages.six.moves import http_cookiejar, urllib
 
 
 class TestVersionCompatibility(object):
@@ -22,3 +24,17 @@ class TestVersionCompatibility(object):
             HTTPConnection('localhost', 12345, source_address='127.0.0.1')
         except TypeError as e:
             pytest.fail('HTTPConnection raised TypeError on source_adddress: %r' % e)
+
+
+class TestCookiejar(object):
+    def test_extract(self):
+        request = urllib.request.Request('http://google.com')
+        cookiejar = http_cookiejar.CookieJar()
+        response = HTTPResponse()
+
+        cookies = ["sessionhash=abcabcabcabcab; path=/; HttpOnly",
+                   "lastvisit=1348253375; expires=Sat, 21-Sep-2050 18:49:35 GMT; path=/"]
+        for c in cookies:
+            response.headers.add('set-cookie', c)
+        cookiejar.extract_cookies(response, request)
+        assert len(cookiejar) == len(cookies)
