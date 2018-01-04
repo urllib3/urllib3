65e6c898a95f20fce496fc57e96600a7cc352db1
Quentin Pradet <quentin.pradet@gmail.com>
2018-01-04 08:22:29 +0400

    Only support SyncBackend in Python 2

--- urllib3/backends/__init__.py
+++ urllib3/backends/__init__.py
@@ -1,3 +1,9 @@
-from .trio_backend import TrioBackend
+from urllib3.packages import six
 from .sync_backend import SyncBackend
-from .twisted_backend import TwistedBackend
+
+__all__ = [SyncBackend]
+
+if six.PY3:
+    from .twisted_backend import TwistedBackend
+    from .trio_backend import TrioBackend
+    __all__ += [TwistedBackend, TrioBackend]
