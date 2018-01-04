ae4d45c7d679ab161fbff75b51af9afe66c24dd8
Quentin Pradet <quentin.pradet@gmail.com>
2018-01-04 08:24:08 +0400

    Add Python 2 demo

---
+++ python2_demo.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+import urllib3
+from urllib3.backends import SyncBackend
+
+URL = "http://httpbin.org/uuid"
+
+print("--- urllib3 using synchronous sockets in Python 2 ---")
+with urllib3.PoolManager(backend=SyncBackend()) as http:
+    print("URL:", URL)
+    r = http.request("GET", URL, preload_content=False)
+    print("Status:", r.status)
+    print("Data: {!r}".format(r.data))
