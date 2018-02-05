82c444d24024aebe63dd25e585fd1d8176a9d44f
Raymond Hettinger <raymond.hettinger@gmail.com>
2018-02-04 23:27:20 -0800

    Separate collections from collections.abc

--- urllib3/_collections.py
+++ urllib3/_collections.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import
-from collections import Mapping, MutableMapping
+try:
+    from collections.abc import Mapping, MutableMapping
+except ImportError:
+    from collections import Mapping, MutableMapping
 try:
     from threading import RLock
 except ImportError:  # Platform-specific: No threads available
