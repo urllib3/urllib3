a1acbcfc41a3f55e58e0f240eedcdf6568de4850
Seth M. Larson <SethMichaelLarson@users.noreply.github.com>
2018-04-24 09:21:58 -0500

    Fix skip logic in SecureTransport tests

--- test/contrib/test_securetransport.py
+++ test/contrib/test_securetransport.py
@@ -6,11 +6,26 @@ import ssl
 import pytest
 
 try:
-    from urllib3.contrib.securetransport import (
-        WrappedSocket, inject_into_urllib3, extract_from_urllib3
-    )
-except ImportError as e:
-    pytestmark = pytest.mark.skip('Could not import SecureTransport: %r' % e)
+    from urllib3.contrib.securetransport import WrappedSocket
+except ImportError:
+    pass
+
+
+def setup_module():
+    try:
+        from urllib3.contrib.securetransport import inject_into_urllib3
+        inject_into_urllib3()
+    except ImportError as e:
+        pytest.skip('Could not import SecureTransport: %r' % e)
+
+
+def teardown_module():
+    try:
+        from urllib3.contrib.securetransport import extract_from_urllib3
+        extract_from_urllib3()
+    except ImportError:
+        pass
+
 
 from ..with_dummyserver.test_https import TestHTTPS, TestHTTPS_TLSv1  # noqa: F401
 from ..with_dummyserver.test_socketlevel import (  # noqa: F401
@@ -18,16 +33,8 @@ from ..with_dummyserver.test_socketlevel import (  # noqa: F401
 )
 
 
-def setup_module():
-    inject_into_urllib3()
-
-
-def teardown_module():
-    extract_from_urllib3()
-
-
 def test_no_crash_with_empty_trust_bundle():
     with contextlib.closing(socket.socket()) as s:
         ws = WrappedSocket(s)
         with pytest.raises(ssl.SSLError):
-            ws._custom_validate(True, b"")
+ws._custom_validate(True, b"")
