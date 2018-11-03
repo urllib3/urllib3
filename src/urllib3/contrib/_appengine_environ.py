557cee6ac61b7d8c55055051a021bb2ef47a960b
Quentin Pradet <quentin.pradet@gmail.com>
2018-11-03 20:27:23 +0100

    Allow missing SERVER_SOFTWARE in appengine checks
    Closes https://github.com/urllib3/urllib3/issues/1470
    
    https://cloud.google.com/appengine/docs/standard/python/tools/using-local-server#detecting_application_runtime_environment

--- src/urllib3/contrib/_appengine_environ.py
+++ src/urllib3/contrib/_appengine_environ.py
@@ -17,12 +17,12 @@ def is_appengine_sandbox():
 
 def is_local_appengine():
     return ('APPENGINE_RUNTIME' in os.environ and
-            'Development/' in os.environ['SERVER_SOFTWARE'])
+            os.getenv('SERVER_SOFTWARE', '').startswith('Development'))
 
 
 def is_prod_appengine():
     return ('APPENGINE_RUNTIME' in os.environ and
-            'Google App Engine/' in os.environ['SERVER_SOFTWARE'] and
+            os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/') and
             not is_prod_appengine_mvms())
 
 
