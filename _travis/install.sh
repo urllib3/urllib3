257efb4b4a6c384630b2b90b6a97382d42a80739
SethMichaelLarson <sethmichaellarson@gmail.com>
2018-11-15 19:27:17 -0600

    Python 3.8 on Travis

--- _travis/install.sh
+++ _travis/install.sh
@@ -40,6 +40,10 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
             pyenv install 3.7-dev
             pyenv global 3.7-dev
             ;;
+        py38)
+            pyenv install 3.8-dev
+            pyenv global 3.8-dev
+            ;;
         pypy*)
             pyenv install "pypy-5.4.1"
             pyenv global "pypy-5.4.1"
