cbe5732ab6d05915cb7c9580d505c36c83cc27c2
Quentin Pradet <quentin@pradet.me>
2018-11-30 20:17:52 +0400

    Use python.org MacPython builds for Travis (#1485)

--- _travis/run.sh
+++ _travis/run.sh
@@ -3,17 +3,8 @@
 set -e
 set -x
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-    # initialize our pyenv
-    PYENV_ROOT="$HOME/.pyenv"
-    PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
-
-    # Use the good OpenSSL for PyOpenSSL. This also links it statically into
-    # cryptography, which more accurately reflects the way the wheel is used.
-    export CRYPTOGRAPHY_OSX_NO_LINK_FLAGS="1"
-    export LDFLAGS="$(brew --prefix openssl@1.1)/lib/libcrypto.a $(brew --prefix openssl@1.1)/lib/libssl.a"
-    export CFLAGS="-I$(brew --prefix openssl@1.1)/include"
+if [[ "$(uname -s)" == "Darwin" && "$TOXENV" == "py27" ]]; then
+    export PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin":$PATH
 fi
 
 tox
