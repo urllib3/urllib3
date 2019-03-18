cbe558b11d10f7e456b62bfba9ce142878d35dea
Quentin Pradet <quentin@pradet.me>
2019-03-19 02:43:30 +0400

    Remove unreachable line within to_input_type() (#1551)

--- src/urllib3/util/url.py
+++ src/urllib3/util/url.py
@@ -196,8 +196,6 @@ def parse_url(url):
     def to_input_type(x):
         if x is None:
             return None
-        elif is_string and isinstance(x, six.binary_type):
-            return x.decode('utf-8')
         elif not is_string and not isinstance(x, six.binary_type):
             return x.encode('utf-8')
         return x
