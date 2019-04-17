d11051936f99afcbd57ef947d5a4643662ab2f14
Seth Michael Larson <sethmichaellarson@gmail.com>
2019-04-17 11:52:22 -0500

    Release 1.24.2

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,12 +1,22 @@
 Changes
 =======
 
+1.24.2 (2019-04-17)
+-------------------
+
+* Don't load system certificates by default when using the ``ssl_context`` parameter.
+
+* Remove Authorization header regardless of case when redirecting to cross-site. (Issue #1510)
+ 
+* Add support for IPv6 addresses in subjectAltName section of certificates. (Issue #1269)
+
+
 1.24.1 (2018-11-02)
 -------------------
 
 * Remove quadratic behavior within ``GzipDecoder.decompress()`` (Issue #1467)
 
-* Restored functionality of `ciphers` parameter for `create_urllib3_context()`. (Issue #1462)
+* Restored functionality of ``ciphers`` parameter for ``create_urllib3_context()``. (Issue #1462)
 
 
 1.24 (2018-10-16)
--- test/with_dummyserver/test_poolmanager.py
+++ test/with_dummyserver/test_poolmanager.py
@@ -123,6 +123,17 @@ class TestPoolManager(HTTPDummyServerTestCase):
 
         self.assertNotIn('Authorization', data)
 
+        r = http.request('GET', '%s/redirect' % self.base_url,
+                         fields={'target': '%s/headers' % self.base_url_alt},
+                         headers={'authorization': 'foo'})
+
+        self.assertEqual(r.status, 200)
+
+        data = json.loads(r.data.decode('utf-8'))
+
+        self.assertNotIn('authorization', data)
+        self.assertNotIn('Authorization', data)
+
     def test_redirect_cross_host_no_remove_headers(self):
         http = PoolManager()
         self.addCleanup(http.clear)
@@ -155,16 +166,20 @@ class TestPoolManager(HTTPDummyServerTestCase):
         self.assertNotIn('X-API-Secret', data)
         self.assertEqual(data['Authorization'], 'bar')
 
-    def test_raise_on_redirect(self):
-        http = PoolManager()
-        self.addCleanup(http.clear)
-
         r = http.request('GET', '%s/redirect' % self.base_url,
-                         fields={'target': '%s/redirect?target=%s/' % (self.base_url,
-                                                                       self.base_url)},
-                         retries=Retry(total=None, redirect=1, raise_on_redirect=False))
+                         fields={'target': '%s/headers' % self.base_url_alt},
+                         headers={'x-api-secret': 'foo',
+                                  'authorization': 'bar'},
+                         retries=Retry(remove_headers_on_redirect=['X-API-Secret']))
 
-        self.assertEqual(r.status, 303)
+        self.assertEqual(r.status, 200)
+
+        data = json.loads(r.data.decode('utf-8'))
+
+        self.assertNotIn('x-api-secret', data)
+        self.assertNotIn('X-API-Secret', data)
+
+        self.assertEqual(data['Authorization'], 'bar')
 
     def test_raise_on_status(self):
         http = PoolManager()
