6014da51b965d7116a2e26013fc575999943c076
Seth Michael Larson <sethmichaellarson@gmail.com>
2019-04-21 18:14:30 -0500

    Install requests

--- _travis/downstream/requests.sh
+++ _travis/downstream/requests.sh
@@ -8,6 +8,7 @@ case "${1}" in
         cd requests
         git rev-parse HEAD
         python -m pip install -r ${TRAVIS_BUILD_DIR}/_travis/downstream/requests-requirements.txt
+        python setup.py install
         ;;
     run)
         cd requests
