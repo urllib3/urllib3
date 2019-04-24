c62203e8eb20a0520d64d260ee487b575a33a688
Seth Michael Larson <sethmichaellarson@gmail.com>
2019-04-24 15:35:31 -0500

    Update test_ssl.py

--- test/test_ssl.py
+++ test/test_ssl.py
@@ -5,11 +5,19 @@ from urllib3.exceptions import SNIMissingWarning
 
 
 @pytest.mark.parametrize('addr', [
+    # IPv6
     '::1',
     '::',
+    'FE80::8939:7684:D84b:a5A4%251',
+
+    # IPv4
     '127.0.0.1',
     '8.8.8.8',
     b'127.0.0.1',
+
+    # IPv6 w/ Zone IDs
+    'FE80::8939:7684:D84b:a5A4%251',
+    b'FE80::8939:7684:D84b:a5A4%251'
     'FE80::8939:7684:D84b:a5A4%19',
     b'FE80::8939:7684:D84b:a5A4%19'
 ])
