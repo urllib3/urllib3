644cd9ae0d835682ef21e7e162e645a456a2fb4a
Thea Flowers <thea@thea.codes>
2019-04-23 21:52:12 -0700

    Attempt to get the python 2 and mac tests working (2)

--- _travis/install.sh
+++ _travis/install.sh
@@ -2,18 +2,21 @@
 
 set -exo pipefail
 
+# Even when testing on Python 2, we need Python 3 for Nox. Ensure it has pip
+# and install Nox into the user profile.
 if ! python3 -m pip --version; then
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     sudo python3 get-pip.py
 fi
+python3 -m pip install --user nox
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     case "${NOX_SESSION}" in
-        tests-2.7) MACPYTHON=2.7.15 ;;
-        tests-3.4) MACPYTHON=3.4.4 ;;
-        tests-3.5) MACPYTHON=3.5.4 ;;
-        tests-3.6) MACPYTHON=3.6.7 ;;
-        tests-3.7) MACPYTHON=3.7.1 ;;
+        test-2.7) MACPYTHON=2.7.15 ;;
+        test-3.4) MACPYTHON=3.4.4 ;;
+        test-3.5) MACPYTHON=3.5.4 ;;
+        test-3.6) MACPYTHON=3.6.7 ;;
+        test-3.7) MACPYTHON=3.7.1 ;;
     esac
 
     MINOR=$(echo $MACPYTHON | cut -d. -f1,2)
@@ -31,6 +34,6 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 fi
 
 if [[ "${TOXENV}" == "gae" ]]; then
-    python3 -m pip install gcp-devrel-py-tools
+    python -m pip install gcp-devrel-py-tools
     gcp-devrel-py-tools download-appengine-sdk "$(dirname ${GAE_SDK_PATH})"
 fi
