37706bb53e9667d8ff669fddf38ef26326f1153f
Zev Goldstein <zev.goldstein@gmail.com>
2019-10-03 13:14:17 -0400

    Stop relying on SERVER_SOFTWARE being set
    Also, deprecate broken functions that rely on GAE_VM

--- src/urllib3/contrib/_appengine_environ.py
+++ src/urllib3/contrib/_appengine_environ.py
@@ -6,27 +6,25 @@ import os
 
 
 def is_appengine():
-    return is_local_appengine() or is_prod_appengine() or is_prod_appengine_mvms()
+    return 'APPENGINE_RUNTIME' in os.environ
 
 
 def is_appengine_sandbox():
-    return is_appengine() and not is_prod_appengine_mvms()
+    """Deprecated. Use is_appengine instead."""
+    return is_appengine()
 
 
 def is_local_appengine():
-    return (
-        "APPENGINE_RUNTIME" in os.environ
-        and "Development/" in os.environ["SERVER_SOFTWARE"]
-    )
+    return (is_appengine() and
+            os.environ.get('SERVER_SOFTWARE','').startswith('Development/'))
 
 
 def is_prod_appengine():
     return (
-        "APPENGINE_RUNTIME" in os.environ
-        and "Google App Engine/" in os.environ["SERVER_SOFTWARE"]
-        and not is_prod_appengine_mvms()
-    )
+      is_appengine() and
+      os.environ.get('SERVER_SOFTWARE','').startswith('Google App Engine/'))
 
 
 def is_prod_appengine_mvms():
-    return os.environ.get("GAE_VM", False) == "true"
+    """Deprecated."""
+    return False
