6ad48b61d74f322a0ae8457f8e3798b542775c96
Quentin Pradet <quentin.pradet@gmail.com>
2019-10-31 15:10:55 +0400

    Cover AppEngine methods about environ
    They always return False in our case, but at least we test that they
    exist and don't crash.

--- test/appengine/test_gae_manager.py
+++ test/appengine/test_gae_manager.py
@@ -168,3 +168,11 @@ class TestGAERetryAfter(test_connectionpool.TestRetryAfter):
         # Disable urlfetch which doesn't respect Retry-After header.
         self.manager = appengine.AppEngineManager(urlfetch_retries=False)
         self.pool = MockPool(self.host, self.port, self.manager)
+
+
+def test_gae_environ():
+    assert not appengine.is_appengine()
+    assert not appengine.is_appengine_sandbox()
+    assert not appengine.is_local_appengine()
+    assert not appengine.is_prod_appengine()
+    assert not appengine.is_prod_appengine_mvms()
