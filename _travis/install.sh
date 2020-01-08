86b34939641f5d29a80eee08232109ba761aea80
Quentin Pradet <quentin.pradet@gmail.com>
2020-01-08 09:34:00 +0400

    Test Python 3.8 on macOS

--- _travis/install.sh
+++ _travis/install.sh
@@ -7,8 +7,14 @@ install_mac_python() {
     local FULL=$1
     local MINOR=$(echo $FULL | cut -d. -f1,2)
     local PYTHON_EXE=/Library/Frameworks/Python.framework/Versions/${MINOR}/bin/python${MINOR}
+    if [[ "$MINOR" == "3.5" ]]; then
+        # The 3.5 python.org macOS build is only compiled with macOS 10.6
+        local COMPILER=10.6
+    else
+        local COMPILER=10.9
+    fi
 
-    curl -Lo macpython.pkg https://www.python.org/ftp/python/${FULL}/python-${FULL}-macosx10.6.pkg
+    curl -Lo macpython.pkg https://www.python.org/ftp/python/${FULL}/python-${FULL}-macosx${COMPILER}.pkg
     sudo installer -pkg macpython.pkg -target /
 
     # The pip in older MacPython releases doesn't support a new enough TLS
@@ -20,10 +26,11 @@ install_mac_python() {
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     # Mac OS setup.
     case "${NOX_SESSION}" in
-        test-2.7) MACPYTHON=2.7.16 ;;
+        test-2.7) MACPYTHON=2.7.17 ;;
         test-3.5) MACPYTHON=3.5.4 ;;  # last binary release
         test-3.6) MACPYTHON=3.6.8 ;;  # last binary release
-        test-3.7) MACPYTHON=3.7.4 ;;
+        test-3.7) MACPYTHON=3.7.6 ;;
+        test-3.8) MACPYTHON=3.8.1 ;;
     esac
 
     install_mac_python $MACPYTHON
