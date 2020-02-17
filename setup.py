cadfd11df1a3e3e9ddb152cae2ef622d5bdce94f
Jon Dufresne <jon.dufresne@gmail.com>
2020-02-16 18:46:24 -0800

    Remove deprecated test arguments from setup.py (#1801)

--- setup.py
+++ setup.py
@@ -69,14 +69,6 @@ setup(
     package_dir={"": "src"},
     requires=[],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
-    tests_require=[
-        # These are a less-specific subset of dev-requirements.txt, for the
-        # convenience of distro package maintainers.
-        "pytest",
-        "mock",
-        "tornado",
-    ],
-    test_suite="test",
     extras_require={
         "brotli": ["brotlipy>=0.6.0"],
         "secure": [
