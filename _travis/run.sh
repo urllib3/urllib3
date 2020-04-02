8e1fe6c6d7707697c600d1946627346ac722df1d
Quentin Pradet <quentin.pradet@gmail.com>
2020-04-02 06:37:27 +0400

    Switch macOS CI to GitHub Actions (#1839)

--- _travis/run.sh
+++ _travis/run.sh
@@ -3,12 +3,7 @@
 set -exo pipefail
 
 if [ -n "${NOX_SESSION}" ]; then
-    if [[ "$(uname -s)" == 'Darwin' ]]; then
-        # Explicitly use python3 on macOS as `nox` is not in the PATH
-        python3 -m nox -s "${NOX_SESSION}"
-    else
-        nox -s "${NOX_SESSION}"
-    fi
+    nox -s "${NOX_SESSION}"
 else
     downstream_script="${TRAVIS_BUILD_DIR}/_travis/downstream/${DOWNSTREAM}.sh"
     if [ ! -x "$downstream_script" ]; then
