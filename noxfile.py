63550371426e4611c83e9785fee291f3c22918b9
Tim Gates <tim.gates@iress.com>
2020-04-19 10:20:09 +1000

    aspell-en

--- noxfile.py
+++ noxfile.py
@@ -109,6 +109,6 @@ def docs(session):
 @nox.session(python=["3.8"])
 def spelling(session):
     subprocess.call(["sudo", "apt-get", "update"])
-    subprocess.call(["sudo", "apt-get", "install", "-y", "aspell"])
+    subprocess.call(["sudo", "apt-get", "install", "-y", "aspell", "aspell-en"])
     session.install("spelling")
     session.run("python", "-m", "spelling")
