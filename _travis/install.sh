fe8f85f3dacb4c4faab948bf67f32a33991ce464
Quentin Pradet <quentin.pradet@gmail.com>
2020-05-29 09:33:43 +0400

    Use nox version that works with Python 3.5.2
    This is the default `python3` shipped by Travis.

--- _travis/install.sh
+++ _travis/install.sh
@@ -10,7 +10,8 @@ set -exo pipefail
 if ! python3 -m pip --version; then
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     sudo python3 get-pip.py
-    sudo python3 -m pip install nox
+    # https://github.com/theacodes/nox/issues/328
+    sudo python3 -m pip install nox==2019.11.9
 else
     # We're not in "dual Python" mode, so we can just install Nox normally.
     python3 -m pip install nox
