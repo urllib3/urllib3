88a9a1c5ef7d32d205105fa99ca24924f7169cfa
Bastiaan Bakker <bbakker@xebia.com>
2020-06-11 15:13:09 +0200

    [1.25] Feature/support env var sslkeylogfile (#1867)

--- test/with_dummyserver/test_https.py
+++ test/with_dummyserver/test_https.py
@@ -698,6 +698,31 @@ class TestHTTPS(HTTPSDummyServerTestCase):
             finally:
                 conn.close()
 
+    @pytest.mark.skipif(
+        not hasattr(ssl.SSLContext, "keylog_filename"),
+        reason="requires OpenSSL 1.1.1+",
+    )
+    @pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python 3.8+")
+    @pytest.mark.skipif(
+        sys.platform == "win32",
+        reason="does not work reliably in Appveyor test enviroment for not yet known reasons",
+    )
+    def test_sslkeylogfile(self, tmpdir, monkeypatch):
+        keylog_file = tmpdir.join("keylogfile.txt")
+        monkeypatch.setenv("SSLKEYLOGFILE", str(keylog_file))
+        with HTTPSConnectionPool(
+            self.host, self.port, ca_certs=DEFAULT_CA
+        ) as https_pool:
+            r = https_pool.request("GET", "/")
+            assert r.status == 200, r.data
+            assert keylog_file.check(file=1), "keylogfile '%s' should exist" % str(
+                keylog_file
+            )
+            assert keylog_file.read().startswith("# TLS secrets log file"), (
+                "keylogfile '%s' should start with '# TLS secrets log file'"
+                % str(keylog_file)
+            )
+
 
 @requiresTLSv1()
 class TestHTTPS_TLSv1(TestHTTPS):
