3ba4755960f54a21a6a1e492aa0d1df8b88aaba6
hodbn <hodbn@users.noreply.github.com>
2020-06-29 22:14:13 -0700

    Raise a meaningful error for an unknown scheme (#1872)

--- test/with_dummyserver/test_poolmanager.py
+++ test/with_dummyserver/test_poolmanager.py
@@ -6,7 +6,7 @@ from dummyserver.server import HAS_IPV6
 from dummyserver.testcase import HTTPDummyServerTestCase, IPv6HTTPDummyServerTestCase
 from urllib3.poolmanager import PoolManager
 from urllib3.connectionpool import port_by_scheme
-from urllib3.exceptions import MaxRetryError
+from urllib3.exceptions import MaxRetryError, URLSchemeUnknown
 from urllib3.util.retry import Retry
 
 from test import LONG_TIMEOUT
@@ -222,6 +222,29 @@ class TestPoolManager(HTTPDummyServerTestCase):
             assert r._pool.num_requests == 2
             assert r._pool.num_connections == 1
             assert len(http.pools) == 1
+
+    def test_unknown_scheme(self):
+        with PoolManager() as http:
+            unknown_scheme = "unknown"
+            unknown_scheme_url = "%s://host" % unknown_scheme
+            with pytest.raises(URLSchemeUnknown) as e:
+                r = http.request("GET", unknown_scheme_url)
+            assert e.value.scheme == unknown_scheme
+            r = http.request(
+                "GET",
+                "%s/redirect" % self.base_url,
+                fields={"target": unknown_scheme_url},
+                redirect=False,
+            )
+            assert r.status == 303
+            assert r.headers.get("Location") == unknown_scheme_url
+            with pytest.raises(URLSchemeUnknown) as e:
+                r = http.request(
+                    "GET",
+                    "%s/redirect" % self.base_url,
+                    fields={"target": unknown_scheme_url},
+                )
+            assert e.value.scheme == unknown_scheme
 
     def test_raise_on_redirect(self):
         with PoolManager() as http:
