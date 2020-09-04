c90013e1945ba4b8f3a2e4fa72ddabccc5710b54
Ezzeri Esa <ezzeri.esa@gmail.com>
2020-09-03 22:29:05 -0700

    Include contrib and filepost.pyi

---
+++ src/urllib3/filepost.pyi
@@ -0,0 +1,15 @@
+from typing import Any, Generator, List, Mapping, Optional, Tuple, Union
+
+from . import fields
+
+Fields = Union[Mapping[str, str], List[Tuple[str]]]
+Iterator = Generator[Tuple[str], None, None]
+
+RequestField = fields.RequestField
+
+writer: Any
+
+def choose_boundary() -> str: ...
+def iter_field_objects(fields: Fields) -> Iterator: ...
+def iter_fields(fields: Fields) -> Iterator: ...
+def encode_multipart_formdata(fields: Fields, boundary: Optional[str]) -> Tuple[str]: ...
