61737c212fc5007381fe087a53830b0677cd5e24
Seth Michael Larson <sethmichaellarson@gmail.com>
2020-10-04 16:19:25 -0500

    Don't set keylog_filename for empty values

--- test/with_dummyserver/test_https.py
+++ test/with_dummyserver/test_https.py
@@ -719,6 +719,18 @@ class TestHTTPS(HTTPSDummyServerTestCase):
                 keylog_file
             )
 
+    @pytest.mark.parametrize("sslkeylogfile", [None, ""])
+    def test_sslkeylogfile_empty(self, monkeypatch, sslkeylogfile):
+        # Assert that an HTTPS connection doesn't error out when given
+        # no SSLKEYLOGFILE or an empty value (ie 'SSLKEYLOGFILE=')
+        if sslkeylogfile is not None:
+            monkeypatch.setenv("SSLKEYLOGFILE", sslkeylogfile)
+        else:
+            monkeypatch.delenv("SSLKEYLOGFILE", raising=False)
+        with HTTPSConnectionPool(self.host, self.port, ca_certs=DEFAULT_CA) as pool:
+            r = pool.request("GET", "/")
+            assert r.status == 200, r.data
+
     def test_alpn_default(self):
         """Default ALPN protocols are sent by default."""
         if not has_alpn() or not has_alpn(ssl.SSLContext):
