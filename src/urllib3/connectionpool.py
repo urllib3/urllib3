3e8888c3f1082202eaf2ac1f1e50e747017af228
Matthew Hughes <matthewhughes934@gmail.com>
2020-10-17 19:41:02 +0100

    Refresh ConnectionPool.urlopen docs
    - Document the 'url' parameter
    - In 'body' param docs: Remove reference to non-existing 'post_url' ,
        and note the support types for this param

--- src/urllib3/connectionpool.py
+++ src/urllib3/connectionpool.py
@@ -551,10 +551,12 @@ class HTTPConnectionPool(ConnectionPool, RequestMethods):
         :param method:
             HTTP request method (such as GET, POST, PUT, etc.)
 
+        :param url:
+            The URL to perform the request on.
+
         :param body:
-            Data to send in the request body (useful for creating
-            POST requests, see HTTPConnectionPool.post_url for
-            more convenience).
+            Data to send in the request body, either `str` or `bytes` (useful
+            for creating POST requests).
 
         :param headers:
             Dictionary of custom headers to send, such as User-Agent,
