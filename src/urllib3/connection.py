978ade337227a5cb8ac4645bbe91084d11f34f0f
Seth Michael Larson <sethmichaellarson@gmail.com>
2020-11-11 09:47:39 -0600

    Add back the HTTPHeaderDict import from 1.25.x

--- src/urllib3/connection.py
+++ src/urllib3/connection.py
@@ -43,6 +43,7 @@ except NameError:  # Python 2:
         pass
 
 
+from ._collections import HTTPHeaderDict  # noqa (historical, removed in v2)
 from ._version import __version__
 from .exceptions import (
     ConnectTimeoutError,
