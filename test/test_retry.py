ba593472e6b7230038f73560dde5321e72471246
Quentin Pradet <quentin.pradet@gmail.com>
2020-11-24 11:27:33 +0400

    Remove deprecated Retry options

--- test/test_retry.py
+++ test/test_retry.py
@@ -1,4 +1,3 @@
-import warnings
 from unittest import mock
 
 import pytest
@@ -13,13 +12,6 @@ from urllib3.exceptions import (
 )
 from urllib3.response import HTTPResponse
 from urllib3.util.retry import RequestHistory, Retry
-
-
-@pytest.fixture(scope="function", autouse=True)
-def no_retry_deprecations():
-    with warnings.catch_warnings(record=True) as w:
-        yield
-    assert len([str(x.message) for x in w if "Retry" in str(x.message)]) == 0
 
 
 class TestRetry:
