d0530788ec0383d2bacf76e60082865716981db2
Paul Bailey <paul.m.bailey@gmail.com>
2021-01-28 15:12:46 -0600

    update python dependency

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
     ],
     package_dir={"": "src"},
     requires=[],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
+    python_requires=">=3.6<4",
     extras_require={
         "brotli": ["brotlipy>=0.6.0"],
         "secure": [
