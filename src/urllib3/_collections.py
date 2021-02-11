9b54a0fe211b2e222977e09a8987dcaeb05d4897
Jess Shapiro <jess@x86.wtf>
2021-02-11 14:31:40 -0800

    rip my bff trailing whitespace

--- src/urllib3/_collections.py
+++ src/urllib3/_collections.py
@@ -414,4 +414,4 @@ class HTTPHeaderDict(MutableMapping[str, str]):
     def has_value_for_header(self, header_name: str, potential_value: str) -> bool:
         if header_name in self:
             return potential_value in self._container[header_name.lower()][1:]
-        return False 
+        return False
