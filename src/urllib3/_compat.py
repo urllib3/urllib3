e0d3b953f97d590cdb824e58ac28d5d926118058
Quentin Pradet <quentin@pradet.me>
2021-03-10 23:11:37 +0400

    Mark '_compat.RLock' as Python 3.6 only
    The threading module is no longer optional in Python 3.7+

--- src/urllib3/_compat.py
+++ src/urllib3/_compat.py
@@ -1,4 +1,4 @@
-class RLock:
+class RLock:  # Python 3.6
     # We shim out a context manager to be used as a compatibility layer
     # if the system `threading` module doesn't have a real `RLock` available.
     def __enter__(self) -> None:
