317a84e9f178a10b9521f98bf02f81fda25cadf8
Dulmandakh <dulmandakh@gmail.com>
2021-03-15 20:40:37 +0800

    extend LifoQueue from python

--- src/urllib3/util/queue.py
+++ src/urllib3/util/queue.py
@@ -2,15 +2,6 @@ import collections
 import queue
 
 
-class LifoQueue(queue.Queue):
+class LifoQueue(queue.LifoQueue):
     def _init(self, _):
         self.queue = collections.deque()
-
-    def _qsize(self, len=len):
-        return len(self.queue)
-
-    def _put(self, item):
-        self.queue.append(item)
-
-    def _get(self):
-        return self.queue.pop()
