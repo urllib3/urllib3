70c24897b16271e9e3225aad0a285884a94051e2
franekmagiera <framagie@gmail.com>
2021-05-01 14:25:29 +0200

    Use .format in setup

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ REQUIRED_PYTHON = (3, 6)
 # This check and everything above must remain compatible with Python 2.7.
 if CURRENT_PYTHON < REQUIRED_PYTHON:
     sys.stderr.write(
-        f"""
+        """
 ==========================
 Unsupported Python version
 ==========================
-This version of urllib3 requires Python {REQUIRED_PYTHON[0]}.{REQUIRED_PYTHON[1]}, but you're trying to
-install it on Python {CURRENT_PYTHON[0]}.{CURRENT_PYTHON[1]}.
+This version of urllib3 requires Python {}.{}, but you're trying to
+install it on Python {}.{}.
 This may be because you are using a version of pip that doesn't
 understand the python_requires classifier. Make sure you
 have pip >= 9.0 and setuptools >= 24.2, then try again:
@@ -28,7 +28,9 @@ This will install the latest version of urllib3 which works on your
 version of Python. If you can't upgrade your pip (or Python), request
 an older version of urllib3:
     $ python -m pip install "urllib3<2"
-"""
+""".format(
+            *(REQUIRED_PYTHON + CURRENT_PYTHON)
+        )
     )
     sys.exit(1)
 
