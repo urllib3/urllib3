d9aff9d04c8a107cfd1658514be6b102cc22ac44
Hasan Ramezani <hasan.r67@gmail.com>
2021-06-19 14:02:40 +0200

    Fix flaky DeprecationWarning tests

--- test/with_dummyserver/test_integration.py
+++ test/with_dummyserver/test_integration.py
@@ -7,29 +7,27 @@ from dummyserver.testcase import HTTPDummyServerTestCase, HTTPSDummyServerTestCa
 
 class TestHTTPIntegration(HTTPDummyServerTestCase):
     def test_requests_integration(self):
-        with pytest.warns(DeprecationWarning) as record:
+        with pytest.warns(DeprecationWarning) as records:
             response = requests.get(f"{self.scheme}://{self.host}:{self.port}")
 
         assert 200 == response.status_code
-        assert 1 == len(record)
         msg = (
             "The 'strict' parameter is no longer needed on Python 3+. "
             "This will raise an error in urllib3 v3.0.0."
         )
-        assert record[0].message.args[0] == msg
+        assert any(record.message.args[0] == msg for record in records)
 
 
 class TestHTTPSIntegration(HTTPSDummyServerTestCase):
     def test_requests_integration(self):
-        with pytest.warns(DeprecationWarning) as record:
+        with pytest.warns(DeprecationWarning) as records:
             response = requests.get(
                 f"{self.scheme}://{self.host}:{self.port}", verify=DEFAULT_CA
             )
 
         assert 200 == response.status_code
-        assert 1 == len(record)
         msg = (
             "The 'strict' parameter is no longer needed on Python 3+. "
             "This will raise an error in urllib3 v3.0.0."
         )
-        assert record[0].message.args[0] == msg
+        assert any(record.message.args[0] == msg for record in records)
