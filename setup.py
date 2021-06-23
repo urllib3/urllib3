b96caa5b4ab63ff88c31e4b01f1c94dc4ad59e89
KOLANICH <kolan_n@mail.ru>
2021-06-23 12:23:13 +0300

    Moved the metadata into setup.cfg Added pyproject.toml Used a well-known external dependency already used by multiple projects to extract version. `setup.py` is kept for now, but unneeded and can be removed.

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # This file is protected via CODEOWNERS
 
-import os
-import re
 import sys
 
 from setuptools import setup
@@ -35,89 +33,5 @@ an older version of urllib3:
     sys.exit(1)
 
 
-base_path = os.path.dirname(__file__)
-
-# Get the version (borrowed from SQLAlchemy)
-with open(os.path.join(base_path, "src", "urllib3", "_version.py")) as fp:
-    VERSION = (
-        re.compile(r""".*__version__ = ["'](.*?)['"]""", re.S).match(fp.read()).group(1)
-    )
-
-
-with open("README.rst", encoding="utf-8") as fp:
-    # Remove reST raw directive from README as they're not allowed on PyPI
-    # Those blocks start with a newline and continue until the next newline
-    mode = None
-    lines = []
-    for line in fp:
-        if line.startswith(".. raw::"):
-            mode = "ignore_nl"
-        elif line == "\n":
-            mode = "wait_nl" if mode == "ignore_nl" else None
-
-        if mode is None:
-            lines.append(line)
-    readme = "".join(lines)
-
-with open("CHANGES.rst", encoding="utf-8") as fp:
-    changes = fp.read()
-
-version = VERSION
-
-setup(
-    name="urllib3",
-    version=version,
-    description="HTTP library with thread-safe connection pooling, file post, and more.",
-    long_description="\n\n".join([readme, changes]),
-    long_description_content_type="text/x-rst",
-    classifiers=[
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Software Development :: Libraries",
-    ],
-    keywords="urllib httplib threadsafe filepost http https ssl pooling",
-    author="Andrey Petrov",
-    author_email="andrey.petrov@shazow.net",
-    url="https://urllib3.readthedocs.io/",
-    project_urls={
-        "Documentation": "https://urllib3.readthedocs.io/",
-        "Code": "https://github.com/urllib3/urllib3",
-        "Issue tracker": "https://github.com/urllib3/urllib3/issues",
-    },
-    license="MIT",
-    packages=[
-        "urllib3",
-        "urllib3.contrib",
-        "urllib3.contrib._securetransport",
-        "urllib3.util",
-    ],
-    package_dir={"": "src"},
-    requires=[],
-    python_requires=">=3.6, <4",
-    extras_require={
-        "brotli": [
-            "brotli>=1.0.9; platform_python_implementation == 'CPython'",
-            "brotlicffi>=0.8.0; platform_python_implementation != 'CPython'",
-        ],
-        "secure": [
-            "pyOpenSSL>=0.14",
-            "cryptography>=1.3.4",
-            "idna>=2.0.0",
-            "certifi",
-        ],
-        "socks": ["PySocks>=1.5.6,<2.0,!=1.5.7"],
-    },
-)
+if __name__ == "__main__":
+    setup()
