e4f689a6a7d7bbde4f1a02448da72cc257e20dff
Derek Higgins <derekh@redhat.com>
2021-07-19 09:24:15 +0100

    Striped square brackets from host IP
    When comparing host IP to a cert subjectAltName->"IP Address"
    ensure were strip square brackets from IPv6 addresses.

--- test/test_connection.py
+++ test/test_connection.py
@@ -117,6 +117,27 @@ class TestConnection:
             )
             assert e._peer_cert == cert
 
+    def test_match_hostname_ip_address_ipv6(self):
+        cert = {"subjectAltName": [("IP Address", "1:2::2:1")]}
+        asserted_hostname = "1:2::2:2"
+        try:
+            with mock.patch("urllib3.connection.log.warning") as mock_log:
+                _match_hostname(cert, asserted_hostname)
+        except CertificateError as e:
+            assert "hostname '1:2::2:2' doesn't match '1:2::2:1'" in str(e)
+            mock_log.assert_called_once_with(
+                "Certificate did not match expected hostname: %s. Certificate: %s",
+                "1:2::2:2",
+                {"subjectAltName": [("IP Address", "1:2::2:1")]},
+            )
+            assert e._peer_cert == cert
+
+    def test_match_hostname_ip_address_ipv6_brackets(self):
+        cert = {"subjectAltName": [("IP Address", "1:2::2:1")]}
+        asserted_hostname = "[1:2::2:1]"
+        match = _match_hostname(cert, asserted_hostname)
+        assert match is None
+
     def test_recent_date(self):
         # This test is to make sure that the RECENT_DATE value
         # doesn't get too far behind what the current date is.
