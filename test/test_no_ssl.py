0c7a1988237c23a4c70e0e496c49cf3e30e142f5
Ran Benita <ran@unusedvar.com>
2021-07-29 14:55:19 +0300

    Fix missing `@classmethod` in tests

--- test/test_no_ssl.py
+++ test/test_no_ssl.py
@@ -23,6 +23,7 @@ class TestWithoutSSL:
         module_stash.stash()
         sys.meta_path.insert(0, ssl_blocker)
 
+    @classmethod
     def teardown_class(cls):
         sys.meta_path.remove(ssl_blocker)
         module_stash.pop()
