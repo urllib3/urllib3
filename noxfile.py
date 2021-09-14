2c9f1ceeb9ca53f997bc7425736bc89822450032
Hasan Ramezani <hasan.r67@gmail.com>
2021-09-14 09:18:02 +0200

    Add type hints to test.with_dummyserver.test_no_ssl

--- noxfile.py
+++ noxfile.py
@@ -169,6 +169,7 @@ def mypy(session: nox.Session) -> None:
         "test/__init__.py",
         "test/with_dummyserver/test_https.py",
         "test/with_dummyserver/test_integration.py",
+        "test/with_dummyserver/test_no_ssl.py",
         "test/conftest.py",
         "test/contrib/test_pyopenssl_dependencies.py",
         "test/contrib/test_socks.py",
