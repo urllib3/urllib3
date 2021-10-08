bb3c2942702b84a9a86057044e61d7b885028471
chetan <44532446+chetanugale@users.noreply.github.com>
2021-10-09 00:06:12 +0530

    [1.26] Vendor ssl.match_hostname to avoid Python 3.10 deprecation warning

--- src/urllib3/connectionpool.py
+++ src/urllib3/connectionpool.py
@@ -36,7 +36,6 @@ from .exceptions import (
 )
 from .packages import six
 from .packages.six.moves import queue
-from .packages.ssl_match_hostname import CertificateError
 from .request import RequestMethods
 from .response import HTTPResponse
 from .util.connection import is_connection_dropped
@@ -45,6 +44,7 @@ from .util.queue import LifoQueue
 from .util.request import set_file_position
 from .util.response import assert_header_parsing
 from .util.retry import Retry
+from .util.ssl_match_hostname import CertificateError
 from .util.timeout import Timeout
 from .util.url import Url, _encode_target
 from .util.url import _normalize_host as normalize_host
