71dfb78712416ebed73a57ad89455f09e970caa4
Tim Pansino <timpansino@gmail.com>
2021-10-18 09:54:48 -0700

    Convert non-relative imports

--- src/urllib3/util/connection.py
+++ src/urllib3/util/connection.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import socket
 
-from urllib3.exceptions import LocationParseError
+from ..exceptions import LocationParseError
 
 from ..contrib import _appengine_environ
 from ..packages import six
