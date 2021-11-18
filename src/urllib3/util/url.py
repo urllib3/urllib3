72ee1db46d2873ed0e6ef46d6f073d45783e89fa
Thomas Dickerson <elfprince13@gmail.com>
2021-11-18 17:14:12 -0500

    Match existing styles
    Addressing review comments on urllib3/urllib3#2487

--- src/urllib3/util/url.py
+++ src/urllib3/util/url.py
@@ -413,8 +413,8 @@ def parse_url(url: str) -> Url:
         if normalize_uri and fragment:
             fragment = _encode_invalid_chars(fragment, _FRAGMENT_CHARS)
 
-    except (ValueError, AttributeError) as parse_exc:
-        raise LocationParseError(source_url) from parse_exc
+    except (ValueError, AttributeError) as e:
+        raise LocationParseError(source_url) from e
 
     # For the sake of backwards compatibility we put empty
     # string values for path if there are any defined values
