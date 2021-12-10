040e7085a6cc54f2eec7242c098bde64e6813fda
Quentin Pradet <quentin.pradet@gmail.com>
2021-12-10 14:52:19 +0400

    Stop report

--- noxfile.py
+++ noxfile.py
@@ -50,7 +50,6 @@ def tests_impl(
         *(session.posargs or ("test/",)),
         env={"PYTHONWARNINGS": "always::DeprecationWarning"},
     )
-    session.run("coverage", "report", "-m")
 
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11", "pypy"])
