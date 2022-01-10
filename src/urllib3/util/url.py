33afb5c96c435ab7118b5f41e26f96c290fef55b
Nate Prewitt <nate.prewitt@gmail.com>
2022-01-10 14:11:53 -0800

    Add authority property to the Url class

--- src/urllib3/util/url.py
+++ src/urllib3/util/url.py
@@ -129,8 +129,29 @@ class Url(
         return uri
 
     @property
+    def authority(self) -> Optional[str]:
+        """
+        Authority component as defined in RFC 3986 3.2.
+        This includes userinfo (auth), host and port.
+
+        i.e.
+            userinfo@host:port
+        """
+        userinfo = self.auth
+        netloc = self.netloc
+        if netloc is None or userinfo is None:
+            return netloc
+        else:
+            return f"{userinfo}@{netloc}"
+
+    @property
     def netloc(self) -> Optional[str]:
-        """Network location including host and port"""
+        """
+        Network location including host and port.
+
+        If you need the equivalent of urllib.parse's ``netloc``,
+        use the ``authority`` property instead.
+        """
         if self.host is None:
             return None
         if self.port:
