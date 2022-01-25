a0ea12fe478c58d7da2e0828f44f1cc6c7dee224
Quentin Pradet <quentin.pradet@gmail.com>
2022-01-25 09:57:44 +0400

    Degrade gracefully for users calling setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.stderr.write(
+    """
+===============================
+Unsupported installation method
+===============================
+
+This version of urllib3 has dropped support for Python 2.7 and no longer supports
+installation with `python setup.py install`.
+
+Please use `python -m pip install .` instead.
+"""
+)
+sys.exit(1)
