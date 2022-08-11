c8e7cbe98f087b0c19687756dcb8b61c2e07768e
IvanLauLinTiong <ivanlau91@hotmail.com>
2022-08-11 12:30:24 +0800

    added type ignore comment to prevent mypy checking error

--- src/urllib3/__init__.py
+++ src/urllib3/__init__.py
@@ -21,7 +21,7 @@ from .util.retry import Retry
 from .util.timeout import Timeout
 
 try:
-    import urllib3_secure_extra  # noqa: F401
+    import urllib3_secure_extra  # type: ignore # noqa: F401
 except ImportError:
     pass
 else:
