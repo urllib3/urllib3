4bbacb842ad52fc0b7d697f10ed2aaf72753c008
Thomas Grainger <tagrain@gmail.com>
2022-11-11 10:27:42 +0000

    catch test_ssl_object_attributes exception

--- test/test_ssltransport.py
+++ test/test_ssltransport.py
@@ -116,10 +116,13 @@ class SingleTLSLayerTestCase(SocketDummyServerTestCase):
     ) -> None:
         def socket_handler(listener: socket.socket) -> None:
             sock = listener.accept()[0]
-            with self.server_context.wrap_socket(sock, server_side=True) as ssock:
-                request = consume_socket(ssock)
-                validate_request(request)
-                ssock.send(sample_response())
+            try:
+                with self.server_context.wrap_socket(sock, server_side=True) as ssock:
+                    request = consume_socket(ssock)
+                    validate_request(request)
+                    ssock.send(sample_response())
+            except ConnectionAbortedError:
+                return
 
         chosen_handler = handler if handler else socket_handler
         self._start_server(chosen_handler)
