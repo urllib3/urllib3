a16efd46bddcb9b8ef7b71d80696e98653112590
Ben Kallus <benjamin.p.kallus.gr@dartmouth.edu>
2023-01-25 21:53:56 -0500

    simplify code further

--- src/urllib3/util/url.py
+++ src/urllib3/util/url.py
@@ -332,7 +332,7 @@ def _normalize_host(host: str | None, scheme: str | None) -> str | None:
 
 
 def _idna_encode(name: str) -> bytes:
-    if name and not name.isascii():
+    if not name.isascii():
         try:
             import idna
         except ImportError:
