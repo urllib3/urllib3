af2cd6bccd1df08a62300fcec079e3fd966725a6
Ben Kallus <49924171+kenballus@users.noreply.github.com>
2023-01-29 19:30:08 +0000

    Fix `_idna_encode()` detection of an ASCII hostname for `\x080`

--- src/urllib3/util/url.py
+++ src/urllib3/util/url.py
@@ -330,7 +330,7 @@ def _normalize_host(host: str | None, scheme: str | None) -> str | None:
 
 
 def _idna_encode(name: str) -> bytes:
-    if name and any([ord(x) > 128 for x in name]):
+    if not name.isascii():
         try:
             import idna
         except ImportError:
