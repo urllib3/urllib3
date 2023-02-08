a06c05cd4bba292ee26e3e9116cff902e0440b52
Ben Kallus <49924171+kenballus@users.noreply.github.com>
2023-02-08 15:19:07 +0000

    Fix _idna_encode handling of '\x80'
    Co-authored-by: Illia Volochii <illia.volochii@gmail.com>

--- src/urllib3/util/url.py
+++ src/urllib3/util/url.py
@@ -303,7 +303,7 @@ def _normalize_host(host, scheme):
 
 
 def _idna_encode(name):
-    if name and any([ord(x) > 128 for x in name]):
+    if name and any(ord(x) >= 128 for x in name):
         try:
             import idna
         except ImportError:
