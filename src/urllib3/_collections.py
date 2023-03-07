7e1d3427ad6a0d50de7eee418f561ab59e2a7484
Hasan Ramezani <hasan.r67@gmail.com>
2023-03-07 14:22:23 +0100

    Update mypy to 1.1.1

--- src/urllib3/_collections.py
+++ src/urllib3/_collections.py
@@ -266,7 +266,7 @@ class HTTPHeaderDict(typing.MutableMapping[str, str]):
             return key.lower() in self._container
         return False
 
-    def setdefault(self, key: str, default: str = "") -> str:
+    def setdefault(self, key: str, default: str = "") -> str:  # type: ignore[override]
         return super().setdefault(key, default)
 
     def __eq__(self, other: object) -> bool:
