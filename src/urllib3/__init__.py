47cce8742b4c5722f6bde4c5de984fcfdfb67f4c
Quentin Pradet <quentin.pradet@gmail.com>
2023-05-10 21:45:20 +0400

    Add quotes around ssl.OPENSSL_VERSION

--- src/urllib3/__init__.py
+++ src/urllib3/__init__.py
@@ -33,14 +33,14 @@ else:
     if not ssl.OPENSSL_VERSION.startswith("OpenSSL "):  # Defensive:
         warnings.warn(
             "urllib3 v2.0 only supports OpenSSL 1.1.1+, currently "
-            f"the 'ssl' module is compiled with {ssl.OPENSSL_VERSION}. "
+            f"the 'ssl' module is compiled with {ssl.OPENSSL_VERSION!r}. "
             "See: https://github.com/urllib3/urllib3/issues/3020",
             exceptions.NotOpenSSLWarning,
         )
     elif ssl.OPENSSL_VERSION_INFO < (1, 1, 1):  # Defensive:
         raise ImportError(
             "urllib3 v2.0 only supports OpenSSL 1.1.1+, currently "
-            f"the 'ssl' module is compiled with {ssl.OPENSSL_VERSION}. "
+            f"the 'ssl' module is compiled with {ssl.OPENSSL_VERSION!r}. "
             "See: https://github.com/urllib3/urllib3/issues/2168"
         )
 
