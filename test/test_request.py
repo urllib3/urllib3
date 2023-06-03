8139a1565c62c3d858cb1befbe214f02c35b80e9
sg3-141-592 <5122866+sg3-141-592@users.noreply.github.com>
2023-06-03 19:28:41 +0000

    Setting warnings for Python 3 only

--- test/test_request.py
+++ test/test_request.py
@@ -1,11 +1,12 @@
 import pytest
+import six
 
 import urllib3
 
-
+@pytest.mark.skipif(six.PY2, reason="This behaviour isn't added when running urllib3 in Python 2",)
 class TestRequestImport(object):
     def test_request_import_warning(self):
         """Ensure an appropriate error is raised to the user
-        if they try and import urllib3.request directly"""
+        if they try and run urllib3.request()"""
         with pytest.warns(UserWarning):
             urllib3.request(1, a=2)
