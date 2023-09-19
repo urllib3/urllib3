1714c01f6976d580594145ea05fdd3959605f826
Quentin Pradet <quentin.pradet@gmail.com>
2023-09-19 23:11:12 +0400

    Undeprecate pyOpenSSL module

--- src/urllib3/contrib/pyopenssl.py
+++ src/urllib3/contrib/pyopenssl.py
@@ -54,20 +54,11 @@ except ImportError:
 import logging
 import ssl
 import typing
-import warnings
 from io import BytesIO
 from socket import socket as socket_cls
 from socket import timeout
 
 from .. import util
-
-warnings.warn(
-    "'urllib3.contrib.pyopenssl' module is deprecated and will be removed "
-    "in urllib3 v2.1.0. Read more in this issue: "
-    "https://github.com/urllib3/urllib3/issues/2680",
-    category=DeprecationWarning,
-    stacklevel=2,
-)
 
 if typing.TYPE_CHECKING:
     from OpenSSL.crypto import X509  # type: ignore[import]
