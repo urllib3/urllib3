fdbac33968020a87f2674239647d1af3f22d05ba
Quentin Pradet <quentin.pradet@gmail.com>
2023-11-08 21:24:29 +0400

    Release 2.1.0

--- src/urllib3/__init__.py
+++ src/urllib3/__init__.py
@@ -32,14 +32,14 @@ except ImportError:
 else:
     if not ssl.OPENSSL_VERSION.startswith("OpenSSL "):  # Defensive:
         warnings.warn(
-            "urllib3 v2.0 only supports OpenSSL 1.1.1+, currently "
+            "urllib3 v2 only supports OpenSSL 1.1.1+, currently "
             f"the 'ssl' module is compiled with {ssl.OPENSSL_VERSION!r}. "
             "See: https://github.com/urllib3/urllib3/issues/3020",
             exceptions.NotOpenSSLWarning,
         )
     elif ssl.OPENSSL_VERSION_INFO < (1, 1, 1):  # Defensive:
         raise ImportError(
-            "urllib3 v2.0 only supports OpenSSL 1.1.1+, currently "
+            "urllib3 v2 only supports OpenSSL 1.1.1+, currently "
             f"the 'ssl' module is compiled with {ssl.OPENSSL_VERSION!r}. "
             "See: https://github.com/urllib3/urllib3/issues/2168"
         )
