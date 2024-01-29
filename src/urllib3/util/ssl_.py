75081e20a360e3066a1c4dd74d2db9949d4b6b90
Jordan Borean <jborean93@gmail.com>
2024-01-29 10:54:35 +1000

    Add fallback for getattr and update comment

--- src/urllib3/util/ssl_.py
+++ src/urllib3/util/ssl_.py
@@ -319,9 +319,9 @@ def create_urllib3_context(
 
     # Enable post-handshake authentication for TLS 1.3, see GH #1634. PHA is
     # necessary for conditional client cert authentication with TLS 1.3.
-    # The attribute is None for OpenSSL <= 1.1.0 or does not exist in older
-    # versions of Python.
-    if getattr(context, "post_handshake_auth") is not None:
+    # The attribute is None for OpenSSL <= 1.1.0 or does not exist when using
+    # an SSLContext created by pyOpenSSL.
+    if getattr(context, "post_handshake_auth", None) is not None:
         context.post_handshake_auth = True
 
     # The order of the below lines setting verify_mode and check_hostname
