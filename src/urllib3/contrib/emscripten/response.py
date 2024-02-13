30e8057cba87b2cc26aa0cef8bb9d96aee65398d
Kevin Paulson <kevin.paulson@mindbridge.ai>
2024-02-13 11:51:32 -0500

    Update EmscriptenHttpResponseWrapper.json()
    * Same as BaseHTTPResponse.json()

--- src/urllib3/contrib/emscripten/response.py
+++ src/urllib3/contrib/emscripten/response.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import json as _json
+import json
 import logging
 import typing
 from contextlib import contextmanager
@@ -211,16 +211,15 @@ class EmscriptenHttpResponseWrapper(BaseHTTPResponse):
 
     def json(self) -> typing.Any:
         """
-        Parses the body of the HTTP response as JSON.
+        Deserializes the body of the HTTP response as a Python object. To use a custom JSON decoder pass the result of :attr:`HTTPResponse.data` to your custom decoder instead.
 
-        To use a custom JSON decoder pass the result of :attr:`HTTPResponse.data` to the decoder.
+        If the body of the HTTP response is not a valid JSON document, a `json.JSONDecodeError` will be raised.
 
-        This method can raise either `UnicodeDecodeError` or `json.JSONDecodeError`.
+        Read more :ref:`here <json_content>`.
 
-        Read more :ref:`here <json>`.
+        :returns: The body of the HTTP response as a Python object.
         """
-        data = self.data.decode("utf-8")
-        return _json.loads(data)
+        return json.loads(self.data)
 
     def close(self) -> None:
         if not self._closed:
