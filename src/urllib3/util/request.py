e928c296e0352bf31e17b5dcd2ee6f70bb83dfb7
정명진 <thinkjin99@gmail.com>
2024-04-13 22:47:45 +0900

    Redesign body_to_chunks function
    body_to_chunks only runs when chunked values is true or Transfer-Encoding header is exists.
    
    In the previous version request function keep using chunking even chunked=False.
    By dividing function to different two parts now we can hanlde chunking by chunked parameter.

--- src/urllib3/util/request.py
+++ src/urllib3/util/request.py
@@ -244,71 +244,33 @@ def body_to_bytes(body: typing.Any, blocksize: int) -> ChunksAndContentLength:
     return ChunksAndContentLength(chunks=chunks, content_length=content_length)
 
 
-def body_to_chunks(
-    body: typing.Any | None, method: str, blocksize: int
-) -> ChunksAndContentLength:
-    """Takes the HTTP request method, body, and blocksize and
-    transforms them into an iterable of chunks to pass to
-    socket.sendall() and an optional 'Content-Length' header.
-
-    A 'Content-Length' of 'None' indicates the length of the body
-    can't be determined so should use 'Transfer-Encoding: chunked'
-    for framing instead.
+def body_to_chunks(body: typing.Any, blocksize: int) -> ChunksAndContentLength:
+    """Takes HTTP body and blocksize and transforms
+    them into an iterable chunks to pass to socket.sendall().
+    This function is only used when header has Transfer-Encoding.
     """
 
-    chunks: typing.Iterable[bytes] | None
+    chunks: typing.Iterable[bytes]
     content_length: int | None
 
-    # No body, we need to make a recommendation on 'Content-Length'
-    # based on whether that request method is expected to have
-    # a body or not.
-    if body is None:
-        chunks = None
-        if method.upper() not in _METHODS_NOT_EXPECTING_BODY:
-            content_length = 0
-        else:
-            content_length = None
-
-    # Bytes or strings become bytes
-    elif isinstance(body, (str, bytes)):
-        chunks = (to_bytes(body),)
-        content_length = len(chunks[0])
+    content_length = None
 
     # File-like object, TODO: use seek() and tell() for length?
-    elif hasattr(body, "read"):
+    if hasattr(body, "read"):
+        chunks = chunk_readable(body, blocksize)
 
-        def chunk_readable() -> typing.Iterable[bytes]:
-            nonlocal body, blocksize
-            encode = isinstance(body, io.TextIOBase)
-            while True:
-                datablock = body.read(blocksize)
-                if not datablock:
-                    break
-                if encode:
-                    datablock = datablock.encode("iso-8859-1")
-                yield datablock
-
-        chunks = chunk_readable()
-        content_length = None
+    elif isinstance(body, (str, bytes)):
+        chunks = (to_bytes(body),)
 
     # Otherwise we need to start checking via duck-typing.
     else:
         try:
-            # Check if the body implements the buffer API.
-            mv = memoryview(body)
+            # Check if the body is an iterable
+            chunks = iter(body)
         except TypeError:
-            try:
-                # Check if the body is an iterable
-                chunks = iter(body)
-                content_length = None
-            except TypeError:
-                raise TypeError(
-                    f"'body' must be a bytes-like object, file-like "
-                    f"object, or iterable. Instead was {body!r}"
-                ) from None
-        else:
-            # Since it implements the buffer API can be passed directly to socket.sendall()
-            chunks = (body,)
-            content_length = mv.nbytes
+            raise TypeError(
+                f"'body' must be a bytes-like object, file-like "
+                f"object, or iterable. Instead was {body!r}"
+            ) from None
 
     return ChunksAndContentLength(chunks=chunks, content_length=content_length)
