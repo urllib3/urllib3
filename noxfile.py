49d2bd6fe5aedcd5fa1016e450823d5e05659e03
Illia Volochii <illia.volochii@gmail.com>
2024-10-27 19:56:08 +0200

    Start testing with Python 3.14

--- noxfile.py
+++ noxfile.py
@@ -51,7 +51,19 @@ def tests_impl(session, extras="socks,secure,brotli"):
 
 
 @nox.session(
-    python=["2.7", "3.6", "3.7", "3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "pypy"]
+    python=[
+        "2.7",
+        "3.6",
+        "3.7",
+        "3.8",
+        "3.9",
+        "3.10",
+        "3.11",
+        "3.12",
+        "3.13",
+        "3.14",
+        "pypy",
+    ]
 )
 def test(session):
     tests_impl(session)
