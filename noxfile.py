4380db47311edab545cbb325e3b2b4cf10727082
Joe Marshall <joe.marshall@nottingham.ac.uk>
2024-10-30 23:36:51 +0000

    remove temporary pyodide browser workaround

--- noxfile.py
+++ noxfile.py
@@ -226,7 +226,7 @@ def emscripten(session: nox.Session, runner: str) -> None:
         dist_dir = Path(os.environ["PYODIDE_ROOT"]) / "dist"
     else:
         # we don't have a build tree
-        pyodide_version = "0.27.0a2"
+        pyodide_version = "0.26.3"
 
         pyodide_artifacts_path = Path(session.cache_dir) / f"pyodide-{pyodide_version}"
         if not pyodide_artifacts_path.exists():
