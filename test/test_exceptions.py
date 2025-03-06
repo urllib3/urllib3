fae02436b535604cda71c3f18d7a26b5f3e1f561
Charles Machalow <csm10495@gmail.com>
2025-03-06 10:42:49 -0800

    Add some context previously lost during the pickle process

--- test/test_exceptions.py
+++ test/test_exceptions.py
@@ -30,6 +30,7 @@ class TestPickle:
         [
             HTTPError(None),
             MaxRetryError(DUMMY_POOL, "", None),
+            MaxRetryError(DUMMY_POOL, "", Exception("Error occured")),
             LocationParseError(""),
             ConnectTimeoutError(None),
             HTTPError("foo"),
@@ -40,13 +41,29 @@ class TestPickle:
             EmptyPoolError(HTTPConnectionPool("localhost"), ""),
             HostChangedError(HTTPConnectionPool("localhost"), "/", 0),
             ReadTimeoutError(HTTPConnectionPool("localhost"), "/", ""),
+            ReadTimeoutError(HTTPConnectionPool("localhost"), "/", "message"),
             NewConnectionError(HTTPConnection("localhost"), ""),
             NameResolutionError("", HTTPConnection("localhost"), socket.gaierror()),
+            NameResolutionError(
+                "host", HTTPConnection("localhost"), socket.gaierror("error")
+            ),
         ],
     )
     def test_exceptions(self, exception: Exception) -> None:
         result = pickle.loads(pickle.dumps(exception))
         assert isinstance(result, type(exception))
+
+        if hasattr(exception, "_message"):
+            assert exception._message == result._message  # type: ignore[attr-defined]
+            assert exception._message in str(result)
+
+        if hasattr(exception, "_host"):
+            # host is likely a string so directly comparable
+            assert exception._host == result._host  # type: ignore[attr-defined]
+
+        if hasattr(exception, "_reason"):
+            # reason is likely an exception so do string comparison instead
+            assert str(exception._reason) == str(result._reason)  # type: ignore[attr-defined]
 
 
 class TestFormat:
