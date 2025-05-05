de5473ae5b5e4d00f7884b27ba9fd0e246bfe668
alexprabhat99 <alexpbara@gmail.com>
2025-05-05 15:02:25 +0530

    fix for failing tests

--- src/urllib3/response.py
+++ src/urllib3/response.py
@@ -1073,12 +1073,12 @@ class HTTPResponse(BaseHTTPResponse):
         return True
 
     def shutdown(self) -> None:
+        if not self._sock_shutdown:
+            raise ValueError("Cannot shutdown socket as self._sock_shutdown is not set")
         if self._connection is None:
             raise RuntimeError(
                 "Cannot shutdown as connection has already been released to the pool"
             )
-        if not self._sock_shutdown:
-            raise ValueError("Cannot shutdown socket as self._sock_shutdown is not set")
         self._sock_shutdown(socket.SHUT_RD)
 
     def close(self) -> None:
