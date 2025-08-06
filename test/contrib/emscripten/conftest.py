7e5bcfc46a6c14e16f1ea3ccd6884f6d9d9a15c3
Illia Volochii <illia.volochii@gmail.com>
2025-08-06 07:13:32 +0300

    Prepare Emscripten tests for Chrome 139 (#3644)
    * Stop using fixtures `selenium_coverage` and `run_from_server` together
    
    * Share the same coverage code in both Selenium fixtures
    
    * Share the same code monkey patching JSPI availability
    
    * Use the `selenium` fixture as the base for all tests against Chrome
    
    https://github.com/pyodide/pytest-pyodide/issues/163#issuecomment-3088631178
    
    * Rename `has_jspi` fixture to `prefer_jspi`
    
    * Deselect and parametrize Emscripten tests properly
    
    * Stop terminating Emscripten tests after the first failure
    
    * Use Chrome Canary in CI to check the changes
    
    * Upgrade Pyodide for CI and some of related test dependencies
    
    * Make tests unapply JSPI patch after a test failure
    
    * Bump Pyodide to 0.28.1 for tests

--- test/contrib/emscripten/conftest.py
+++ test/contrib/emscripten/conftest.py
@@ -57,19 +57,75 @@ class PyodideServerInfo:
     pyodide_dist_dir: Path
 
 
+def _get_coverage_code() -> tuple[str, str]:
+    begin = textwrap.dedent(
+        """
+        import coverage
+
+        _coverage = coverage.Coverage(source_pkgs=["urllib3"])
+        _coverage.start()
+        """
+    )
+    end = textwrap.dedent(
+        """
+        _coverage.stop()
+        _coverage.save()
+
+        _coverage_datafile = open(".coverage", "rb")
+        _coverage_outdata = _coverage_datafile.read()
+        _coverage_datafile.close()
+
+        # avoid polluting main namespace too much
+        import js as _coverage_js
+        # convert to js Array (as default conversion is TypedArray which does
+        # bad things in firefox)
+        _coverage_js.Array.from_(_coverage_outdata)
+        """
+    )
+    return begin, end
+
+
+def _get_jspi_monkeypatch_code(runtime: str, prefer_jspi: bool) -> tuple[str, str]:
+    """
+    Return code to make Pyodide think JSPI is disabled in Chrome when a
+    test needs this to check some code paths.
+    """
+    if runtime != "chrome" or prefer_jspi:
+        return "", ""
+    monkeypatch_code = textwrap.dedent(
+        """
+        import pyodide.ffi
+
+        original_can_run_sync = pyodide.ffi.can_run_sync
+        if pyodide.ffi.can_run_sync():
+            pyodide.ffi.can_run_sync = lambda: False
+        """
+    )
+    unmonkeypatch_code = "pyodide.ffi.can_run_sync = original_can_run_sync"
+    return monkeypatch_code, unmonkeypatch_code
+
+
 @pytest.fixture()
 def selenium_with_jspi_if_possible(
-    request: pytest.FixtureRequest, runtime: str, has_jspi: bool
+    request: pytest.FixtureRequest, runtime: str, prefer_jspi: bool
 ) -> Generator[Any]:
-    if runtime.startswith("firefox") or not has_jspi:
-        fixture_name = "selenium"
-        with_jspi = False
-    else:
+    if runtime == "node" and prefer_jspi:
         fixture_name = "selenium_jspi"
-        with_jspi = True
+    else:
+        fixture_name = "selenium"
     selenium_obj = request.getfixturevalue(fixture_name)
-    selenium_obj.with_jspi = with_jspi
-    yield selenium_obj
+
+    jspi_monkeypatch_code, jspi_unmonkeypatch_code = _get_jspi_monkeypatch_code(
+        runtime, prefer_jspi
+    )
+    if jspi_monkeypatch_code:
+        selenium_obj.run_async(jspi_monkeypatch_code)
+
+    try:
+        yield selenium_obj
+    finally:
+        if jspi_unmonkeypatch_code:
+            selenium_obj.run_async(jspi_unmonkeypatch_code)
 
 
 @pytest.fixture()
@@ -84,29 +140,8 @@ def selenium_coverage(
         result = self.run_js(
             f'await pyodide.loadPackage("http://{testserver_http.http_host}:{testserver_http.http_port}/dist/urllib3.whl")'
         )
-        if not self.with_jspi:
-            # force Chrome to execute the current test without JSPI
-            # even though it is always enabled in
-            # chrome >= 137. We do this by monkeypatching
-            # pyodide.ffi.can_run_sync
-            self.run_async(
-                """
-                import pyodide.ffi
-                if pyodide.ffi.can_run_sync():
-                    pyodide.ffi.can_run_sync = lambda: False
-                """
-            )
-
         print("Installed package:", result)
-        self.run_js(
-            """
-            await pyodide.loadPackage("coverage")
-            await pyodide.runPythonAsync(`import coverage
-_coverage= coverage.Coverage(source_pkgs=['urllib3'])
-_coverage.start()
-        `
-        )"""
-        )
+        self.run_js("await pyodide.loadPackage('coverage')")
 
     setattr(
         selenium_with_jspi_if_possible,
@@ -117,23 +152,18 @@ _coverage.start()
     )
 
     selenium_with_jspi_if_possible._install_packages()
+
+    coverage_begin, coverage_end = _get_coverage_code()
+    selenium_with_jspi_if_possible.run_js(
+        f"await pyodide.runPythonAsync(`{coverage_begin}`)"
+    )
+
     yield selenium_with_jspi_if_possible
+
     # on teardown, save _coverage output
     coverage_out_binary = bytes(
         selenium_with_jspi_if_possible.run_js(
-            """
-return await pyodide.runPythonAsync(`
-_coverage.stop()
-_coverage.save()
-_coverage_datafile = open(".coverage","rb")
-_coverage_outdata = _coverage_datafile.read()
-# avoid polluting main namespace too much
-import js as _coverage_js
-# convert to js Array (as default conversion is TypedArray which does
-# bad things in firefox)
-_coverage_js.Array.from_(_coverage_outdata)
-`)
-    """
+            f"return await pyodide.runPythonAsync(`{coverage_end}`)"
         )
     )
     with open(f"{_get_coverage_filename('.coverage.emscripten.')}", "wb") as outfile:
@@ -142,53 +172,22 @@ _coverage_js.Array.from_(_coverage_outdata)
 
 class ServerRunnerInfo:
     def __init__(
-        self, host: str, port: int, selenium: Any, dist_dir: Path, has_jspi: bool
+        self, host: str, port: int, selenium: Any, dist_dir: Path, prefer_jspi: bool
     ) -> None:
         self.host = host
         self.port = port
         self.selenium = selenium
         self.dist_dir = dist_dir
-        self.has_jspi = has_jspi
+        self.prefer_jspi = prefer_jspi
 
     def run_webworker(self, code: str) -> Any:
         if isinstance(code, str) and code.startswith("\n"):
             # we have a multiline string, fix indentation
             code = textwrap.dedent(code)
 
-        # add coverage collection to this code
-        coverage_init_code = textwrap.dedent(
-            """
-            import coverage
-            _coverage= coverage.Coverage(source_pkgs=['urllib3'])
-            _coverage.start()
-            """
-        )
-
-        # Monkeypatch pyodide to force disable JSPI in newer chrome
-        # so those code paths get tested
-        if self.has_jspi is False:
-            jspi_fix_code = textwrap.dedent(
-                """
-                import pyodide.ffi
-                if pyodide.ffi.can_run_sync():
-                    pyodide.ffi.can_run_sync = lambda: False
-                """
-            )
-        else:
-            jspi_fix_code = ""
-
-        coverage_end_code = textwrap.dedent(
-            """
-            _coverage.stop()
-            _coverage.save()
-            _coverage_datafile = open(".coverage","rb")
-            _coverage_outdata = _coverage_datafile.read()
-            # avoid polluting main namespace too much
-            import js as _coverage_js
-            # convert to js Array (as default conversion is TypedArray which does
-            # bad things in firefox)
-            _coverage_js.Array.from_(_coverage_outdata)
-            """
+        coverage_init_code, coverage_end_code = _get_coverage_code()
+        jspi_monkeypatch_code, jspi_unmonkeypatch_code = _get_jspi_monkeypatch_code(
+            self.selenium.browser, self.prefer_jspi
         )
 
         # the ordering of these code blocks is important - makes sure
@@ -196,9 +195,13 @@ class ServerRunnerInfo:
         code = (
             coverage_init_code
             + "\n"
-            + jspi_fix_code
+            + jspi_monkeypatch_code
             + "\n"
-            + code
+            + "try:\n"
+            + textwrap.indent(code, "    ")
+            + "\n"
+            + "finally:\n"
+            + textwrap.indent(jspi_unmonkeypatch_code or "pass", "    ")
             + "\n"
             + coverage_end_code
         )
@@ -250,51 +253,88 @@ class ServerRunnerInfo:
 # we are at the same origin as web requests to server_host
 @pytest.fixture()
 def run_from_server(
-    selenium_coverage: Any, testserver_http: PyodideServerInfo
+    selenium: Any, testserver_http: PyodideServerInfo, prefer_jspi: bool
 ) -> Generator[ServerRunnerInfo]:
-    if selenium_coverage.browser != "node":
+    if selenium.browser != "node":
         # on node, we don't need to be on the same origin
         # so we can ignore all this
         addr = f"https://{testserver_http.http_host}:{testserver_http.https_port}/pyodide/test.html"
-        selenium_coverage.goto(addr)
-        selenium_coverage.javascript_setup()
-        selenium_coverage.load_pyodide()
-        selenium_coverage.initialize_pyodide()
-        selenium_coverage.save_state()
-        selenium_coverage.restore_state()
-        selenium_coverage._install_packages()
+        selenium.goto(addr)
+        selenium.javascript_setup()
+        selenium.load_pyodide()
+        selenium.initialize_pyodide()
+        selenium.save_state()
+        selenium.restore_state()
     dist_dir = testserver_http.pyodide_dist_dir
     yield ServerRunnerInfo(
         testserver_http.http_host,
         testserver_http.https_port,
-        selenium_coverage,
+        selenium,
         dist_dir,
-        selenium_coverage.with_jspi,
+        prefer_jspi,
     )
 
 
-def pytest_generate_tests(metafunc: pytest.Metafunc) -> None:
-    """Generate tests with WebAssembly JavaScript Promise Integration both
-     enabled and disabled depending on browser/node.js support for features.
-     Also drops any test that requires a browser or web-workers in Node.js.
-    ).
+def pytest_collection_modifyitems(
+    config: pytest.Config, items: list[pytest.Item]
+) -> None:
     """
-    if "has_jspi" in metafunc.fixturenames:
-        can_run_with_jspi = False
-        can_run_without_jspi = False
+    A pytest hook modifying collected test items for Emscripten.
+    """
+    selected_tests = []
+    deselected_tests = []
+
+    runtime = config.getoption("--runtime", default=None)
+    if not runtime:
+        return
+
+    for item in items:
+        # Deselect tests which Node.js cannot run.
+        if runtime.startswith("node"):
+            if (
+                item.get_closest_marker("webworkers")
+                or item.get_closest_marker("in_webbrowser")
+                or item.get_closest_marker("without_jspi")
+            ):
+                deselected_tests.append(item)
+                continue
+        # Tests marked with `in_webbrowser` are only for Node.js.
+        elif item.get_closest_marker("node_without_jspi"):
+            deselected_tests.append(item)
+            continue
+
+        # Firefox cannot run JSPI tests.
+        if runtime.startswith("firefox") and item.get_closest_marker("with_jspi"):
+            deselected_tests.append(item)
+            continue
+
+        selected_tests.append(item)
+
+    config.hook.pytest_deselected(items=deselected_tests)
+    items[:] = selected_tests
+
+
+def pytest_generate_tests(metafunc: pytest.Metafunc) -> None:
+    """
+    A pytest hook generating parametrized calls to a test function.
+    """
+
+    # Set proper `prefer_jspi` values for tests with WebAssembly
+    # JavaScript Promise Integration both enabled and disabled depending
+    # on browser/Node.js support for features.
+    if "prefer_jspi" in metafunc.fixturenames:
         # node only supports JSPI and doesn't support workers or
         # webbrowser specific tests
         if metafunc.config.getoption("--runtime").startswith("node"):
-            if (
-                metafunc.definition.get_closest_marker("webworkers") is None
-                and metafunc.definition.get_closest_marker("in_webbrowser") is None
-            ):
-                can_run_with_jspi = True
             if metafunc.definition.get_closest_marker("node_without_jspi"):
-                can_run_without_jspi = True
                 can_run_with_jspi = False
+                can_run_without_jspi = True
+            else:
+                can_run_with_jspi = True
+                can_run_without_jspi = False
         # firefox doesn't support JSPI
         elif metafunc.config.getoption("--runtime").startswith("firefox"):
+            can_run_with_jspi = False
             can_run_without_jspi = True
         else:
             # chrome supports JSPI on or off
@@ -313,4 +353,4 @@ def pytest_generate_tests(metafunc: pytest.Metafunc) -> None:
             jspi_options.append(False)
         if can_run_with_jspi:
             jspi_options.append(True)
-        metafunc.parametrize("has_jspi", jspi_options)
+        metafunc.parametrize("prefer_jspi", jspi_options)
