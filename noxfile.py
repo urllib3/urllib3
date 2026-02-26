ac2ab40fb6520f792443a2acdfca646219c5391a
Illia Volochii <illia.volochii@gmail.com>
2026-02-26 12:12:50 +0200

    Bump the minimum supported pyOpenSSL version to 19.0.0 (#3777)

--- noxfile.py
+++ noxfile.py
@@ -22,6 +22,7 @@ def tests_impl(
     integration: bool = False,
     pytest_extra_args: list[str] = [],
     dependency_group: str = "dev",
+    no_default_groups: bool = False,
 ) -> None:
     # Retrieve sys info from the Python implementation under test
     # to avoid enabling memray when nox runs under CPython but tests PyPy
@@ -43,6 +44,7 @@ def tests_impl(
         "uv",
         "sync",
         "--frozen",
+        *("--no-default-groups",) if no_default_groups else (),
         "--group",
         dependency_group,
         *(f"--extra={extra}" for extra in (extras.split(",") if extras else ())),
@@ -125,6 +127,22 @@ def test_integration(session: nox.Session) -> None:
     """Run integration tests"""
     session.env["UV_PROJECT_ENVIRONMENT"] = session.virtualenv.location
     tests_impl(session, integration=True)
+
+
+# We use 3.13 in GitHub Actions.
+@nox.session(python="3.13")
+def test_min_pyopenssl(session: nox.Session) -> None:
+    """
+    Check that we do not break with the documented minimum supported
+    pyOpenSSL version.
+    """
+    session.env["UV_PROJECT_ENVIRONMENT"] = session.virtualenv.location
+    tests_impl(
+        session,
+        pytest_extra_args=["-k", "pyopenssl"],
+        dependency_group="dev-min-pyopenssl",
+        no_default_groups=True,
+    )
 
 
 @nox.session(python="3")
