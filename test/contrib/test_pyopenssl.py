afd2bd7ba133cce81ac0b72e1187425bdfcdd0b9
dumbCodesOnly <225476909+dumbCodesOnly@users.noreply.github.com>
2026-07-16 20:20:16 +0330

    Fix missing imports (Encoding, Any) in new pyopenssl tests

--- test/contrib/test_pyopenssl.py
+++ test/contrib/test_pyopenssl.py
@@ -7,10 +7,12 @@ import pytest
 
 try:
     import datetime
+    from typing import Any
 
     from cryptography import x509
     from cryptography.hazmat.primitives import hashes
     from cryptography.hazmat.primitives.asymmetric import rsa
+    from cryptography.hazmat.primitives.serialization import Encoding
     from cryptography.x509.oid import NameOID
     from OpenSSL.crypto import FILETYPE_PEM, load_certificate
 
