d99255025394cb3272a15f8e2c3e460fee7c91c9
Illia Volochii <illia.volochii@gmail.com>
2026-09-18 01:21:03 +0300

    Run tests in parallel

--- test/with_dummyserver/test_no_ssl.py
+++ test/with_dummyserver/test_no_ssl.py
@@ -1,37 +1,31 @@
 """
 Test connections without the builtin ssl module
 
-Note: Import urllib3 inside the test functions to get the importblocker to work
+Note: Import urllib3 inside the test functions so it is loaded without SSL support.
 """
 
 from __future__ import annotations
 
 import pytest
 
-import urllib3
-from dummyserver.testcase import (
-    HTTPSHypercornDummyServerTestCase,
-    HypercornDummyServerTestCase,
-)
-from urllib3.exceptions import InsecureRequestWarning
+from dummyserver.testcase import HypercornDummyServerTestCase
 
-from ..test_no_ssl import TestWithoutSSL
+pytestmark = pytest.mark.usefixtures("without_ssl")
 
 
-class TestHTTPWithoutSSL(HypercornDummyServerTestCase, TestWithoutSSL):
+class TestHTTPWithoutSSL(HypercornDummyServerTestCase):
     def test_simple(self) -> None:
+        import urllib3
+
         with urllib3.HTTPConnectionPool(self.host, self.port) as pool:
             r = pool.request("GET", "/")
             assert r.status == 200, r.data
 
 
-class TestHTTPSWithoutSSL(HTTPSHypercornDummyServerTestCase, TestWithoutSSL):
+class TestHTTPSWithoutSSL:
     def test_simple(self) -> None:
-        with urllib3.HTTPSConnectionPool(
-            self.host, self.port, cert_reqs="NONE"
-        ) as pool:
-            with pytest.warns(InsecureRequestWarning):
-                try:
-                    pool.request("GET", "/")
-                except urllib3.exceptions.SSLError as e:
-                    assert "SSL module is not available" in str(e)
+        import urllib3
+
+        with urllib3.HTTPSConnectionPool("localhost") as pool:
+            with pytest.raises(ImportError, match="SSL module is not available"):
+                pool.request("GET", "/")
